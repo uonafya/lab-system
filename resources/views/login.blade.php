1e66b5064bc14b8f9735eac6e13c7512ef9a07d5
Bakasa Joshua <joshua.bakasa@dataposit.co.ke>
2018-02-01 14:00:26 +0300

    some minor changes to the login page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,26 +1,27 @@
 @extends('layouts.auth')
 
 @section('content')
+
 <div class="row">
         <div class="col-md-12">
-            <div class="text-center m-b-md">
+            <!-- <div class="text-center m-b-md">
                 <img src="{{ asset('img/nascoplogo.png') }}">
-            </div>
-            <div class="hpanel">
+            </div> -->
+            <div class="hpanel" style="width: 430px;">
                 <div class="panel-body">
                     <form action="#" id="loginForm">
                         <div class="form-group">
-                            <label class="control-label" for="username">Username</label>
-                            <input type="text" placeholder="Username" title="Please enter you username" required="" value="" name="username" id="username" class="form-control">
+                            <label class="control-label" for="username" style="color: black;">Username:</label>
+                            <div class="input-group m-b"><span class="input-group-addon"><span class="fa fa-user-o"></span></span> <input  type="text" placeholder="Username" title="Please enter you username" required="" value="" name="username" id="username" class="form-control"></div>
                         </div>
                         <div class="form-group">
-                            <label class="control-label" for="password">Password</label>
-                            <input type="password" title="Please enter your password" placeholder="Password" required="" value="" name="password" id="password" class="form-control">
+                            <label class="control-label" for="password" style="color: black;">Password:</label>
+                            <div class="input-group m-b"><span class="input-group-addon"><span class="pe-7s-unlock"></span></span> <input type="password" title="Please enter your password" placeholder="Password" required="" value="" name="password" id="password" class="form-control"></div>
                         </div>
-                        <button class="btn btn-primary btn-block">Login</button>
+                        <button type="submit" class="btn btn-danger btn-block" style="background-color: #16A085;border-color: #16A085;margin-top: 2em;">Login</button>
                     </form>
-                    <div class="text-center m-b-md facility-login">
-                        <a href="#"><p>Click <strong class="font-extra-bold font-uppercase">here</strong> for facility login</p></a>
+                    <div class="text-center m-b-md">
+                        <a href="#" style="color: white;"><button class="btn btn-primary btn-block" style="margin-top: 2em;">Click <strong class="font-extra-bold font-uppercase">here</strong> for facility login</button></a>
                     </div>
                 </div>
             </div>
