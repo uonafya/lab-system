59978932066eaaf176f7e1b10abb6a7eeb2093ef
Bakasa Joshua <joshua.bakasa@dataposit.co.ke>
2018-02-22 16:56:33 +0300

    Played with the sidebar abit

--- resources/views/layouts/sidenav.blade.php
+++ resources/views/layouts/sidenav.blade.php
@@ -1,12 +1,15 @@
 <aside id="menu">
     <div id="navigation">
         <ul class="nav" id="side-menu">
+        @if (auth()->user()->user_type_id == 1 || auth()->user()->user_type_id == 4)
             <li class="active">
                 <a href="{{ url('home') }}"> <span class="nav-label">Tasks</span> <span class="label label-success pull-right">12</span> </a>
             </li>
+        @endif
             <li>
                 <a href="#"> <span class="nav-label">Dashboard</span> </a>
             </li>
+        @if (auth()->user()->user_type_id == 1 || auth()->user()->user_type_id == 4)
             <li>
                 <a href="#"><span class="nav-label">Samples</span><span class="fa arrow"></span> </a>
                 <ul class="nav nav-second-level">
@@ -23,6 +26,8 @@
                     <li><a href="#">Create Abbott Worksheet(96)</a></li>
                 </ul>
             </li>
+        @endif
+        @if (auth()->user()->user_type_id == 1)
             <li>
                 <a href="#"><span class="nav-label">Results</span><span class="label label-warning pull-right">20<span class="fa arrow"></span></span></a>
                 <ul class="nav nav-second-level">
@@ -31,18 +36,37 @@
                 </ul>
             </li>
             <li>
-                <a href="#"> <span class="nav-label">Verify Batch Entry</span><span class="label label-danger pull-right">20</span> </a>
-            </li>
-            <li>
                 <a href="#"><span class="nav-label">Requisitions</span><span class="fa arrow"></span> </a>
                 <ul class="nav nav-second-level">
                     <li><a href="#">Make Requisition</a></li>
                     <li><a href="#">Requisition List</a></li>
                 </ul>
             </li>
+        @endif
+        @if (auth()->user()->user_type_id == 1 || auth()->user()->user_type_id == 4)
+            <li>
+                <a href="#"> <span class="nav-label">Verify Batch Entry</span><span class="label label-danger pull-right">20</span> </a>
+            </li>
+        @endif
+        @if (auth()->user()->user_type_id == 2 || auth()->user()->user_type_id == 4)
+        	<li>
+                <a href="#"> <span class="nav-label">Facilities</span></a>
+            </li>
+        @endif
+        @if (auth()->user()->user_type_id == 2)
+        	<li>
+                <a href="#"> <span class="nav-label">Districts</span></a>
+            </li>
+            <li>
+                <a href="#"> <span class="nav-label">User Activity Log</span></a>
+            </li>
+        @endif
+        @if (auth()->user()->user_type_id == 1 || auth()->user()->user_type_id == 4)
             <li>
                 <a href="#"> <span class="nav-label">KITS</span></a>
             </li>
+        @endif
+        @if (auth()->user()->user_type_id == 1 || auth()->user()->user_type_id == 2)
             <li>
                 <a href="#"><span class="nav-label">SMS Printer</span><span class="fa arrow"></span> </a>
                 <ul class="nav nav-second-level">
@@ -50,15 +74,21 @@
                     <li><a href="#">View SMS Printer</a></li>
                 </ul>
             </li>
+        @endif
             <li>
                 <a href="https://eid.nascop.org"> <span class="nav-label">NASCOP</span><span class="label label-success pull-right">National</span></a>
             </li>
+        @if (auth()->user()->user_type_id == 1 || auth()->user()->user_type_id == 4)
             <li>
                 <a href="#"> <span class="nav-label">Add Quarterly Kit Deliveries</span> <span class="label label-success pull-right">Special</span></a>
             </li>
+        @endif
+        @if (auth()->user()->user_type_id != 5)
             <li>
                 <a href="#"> <span class="nav-label">Change Password</span></a>
             </li>
+        @endif
+        @if (auth()->user()->user_type_id != 2)
             <li>
                 <a href="#"> <span class="nav-label">User Manual</span></a>
             </li>
@@ -69,6 +99,7 @@
                     <li><a href="#">VL Form</a></li>
                 </ul>
             </li>
+        @endif
         </ul>
     </div>
 </aside>
