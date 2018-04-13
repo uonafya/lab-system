3182b92b8324a6a51ebf8a99c2a8262574763be4
mwendakith <joelkith@gmail.com>
2018-04-13 15:29:12 +0300

    Fixes

--- app/Http/Controllers/WorksheetController.php
+++ app/Http/Controllers/WorksheetController.php
@@ -70,7 +70,7 @@ class WorksheetController extends Controller
             return $worksheet;
         });
 
-        return view('tables.worksheets', ['worksheets' => $worksheets, 'myurl' => url('worksheet/index/' . $state . '/')]);
+        return view('tables.worksheets', ['worksheets' => $worksheets, 'myurl' => url('worksheet/index/' . $state . '/')])->with('pageTitle', 'Worksheets');
 
         // $table_rows = "";
 
