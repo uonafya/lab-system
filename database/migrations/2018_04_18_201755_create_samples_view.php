57dd7981d9ea17edd4121cf10668fb99fb71c955
mwendakith <joelkith@gmail.com>
2018-05-03 22:52:29 +0300

    Copied change made to view.sql to the respective migrations

--- database/migrations/2018_04_18_201755_create_samples_view.php
+++ database/migrations/2018_04_18_201755_create_samples_view.php
@@ -16,12 +16,14 @@ class CreateSamplesView extends Migration
         DB::statement("
         CREATE OR REPLACE VIEW samples_view AS
         (
-          SELECT s.*, b.national_batch_id, b.highpriority, b.datereceived, b.datedispatched, b.site_entry, b.lab_id, b.facility_id, b.input_complete,
+          SELECT s.*, b.national_batch_id, b.highpriority, b.datereceived, b.datedispatched, b.site_entry, b.lab_id, f.facilitycode, b.facility_id, b.input_complete,
           p.national_patient_id, p.patient, p.sex, p.dob, p.mother_id, p.entry_point
 
           FROM samples s
             JOIN batches b ON b.id=s.batch_id
             JOIN patients p ON p.id=s.patient_id
+            LEFT JOIN facilitys f ON f.id=b.facility_id
+
 
         );
         ");
--- database/migrations/2018_04_18_201803_create_viralsamples_view.php
+++ database/migrations/2018_04_18_201803_create_viralsamples_view.php
@@ -16,12 +16,13 @@ class CreateViralsamplesView extends Migration
         DB::statement("
         CREATE OR REPLACE VIEW viralsamples_view AS
         (
-          SELECT s.*, b.national_batch_id, b.highpriority, b.datereceived, b.datedispatched, b.site_entry, b.lab_id, b.facility_id, b.input_complete,
+          SELECT s.*, b.national_batch_id, b.highpriority, b.datereceived, b.datedispatched, b.site_entry, b.lab_id, f.facilitycode, b.facility_id, b.input_complete,
           p.national_patient_id, p.patient, p.patient_name, p.initiation_date, p.sex, p.dob
 
           FROM viralsamples s
             JOIN viralbatches b ON b.id=s.batch_id
             JOIN viralpatients p ON p.id=s.patient_id
+            LEFT JOIN facilitys f ON f.id=b.facility_id
 
         );
         ");
