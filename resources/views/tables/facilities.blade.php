89f80fbe6b0714dc1cc315cc5891c3f7a02e233a
mwendakith <joelkith@gmail.com>
2018-05-31 16:57:08 +0300

    POC changes

--- resources/views/tables/facilities.blade.php
+++ resources/views/tables/facilities.blade.php
@@ -34,34 +34,37 @@
 		            </div>
             	</div>
                 <div class="panel-body">
-            	    <table class="table table-striped table-bordered table-hover data-table" style="font-size: 10px;" >
-                        <thead>
-                            <tr class="colhead">
-                                @php
-                                    echo $columns 
-                                @endphp
-                                <!-- <th>MFL</th>
-                                <th>Facility Name</th>
-                                <th>County</th>
-                                <th>District</th>
-                                <th>Facility Phone 1</th>
-                                <th>Facility Phone 2</th>
-                                <th>Facility Email</th>
-                                <th>Facility SMS Printer #</th>
-                                <th>Contact Person Names</th>
-                                <th>Contact Phone 1</th>
-                                <th>Contact Phone 2</th>
-                                <th>Contact Email</th>
-                                <th>G4S Branch</th>
-                                <th>Task</th> -->
-                            </tr>
-                        </thead>
-                        <tbody>
-                        	@php 
-                        		echo $row 
-                        	@endphp
-                        </tbody>
-                    </table>
+                    <div class="table-responsive">
+
+                	    <table class="table table-striped table-bordered table-hover data-table" style="font-size: 10px;" >
+                            <thead>
+                                <tr class="colhead">
+                                    @php
+                                        echo $columns 
+                                    @endphp
+                                    <!-- <th>MFL</th>
+                                    <th>Facility Name</th>
+                                    <th>County</th>
+                                    <th>District</th>
+                                    <th>Facility Phone 1</th>
+                                    <th>Facility Phone 2</th>
+                                    <th>Facility Email</th>
+                                    <th>Facility SMS Printer #</th>
+                                    <th>Contact Person Names</th>
+                                    <th>Contact Phone 1</th>
+                                    <th>Contact Phone 2</th>
+                                    <th>Contact Email</th>
+                                    <th>G4S Branch</th>
+                                    <th>Task</th> -->
+                                </tr>
+                            </thead>
+                            <tbody>
+                            	@php 
+                            		echo $row 
+                            	@endphp
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
