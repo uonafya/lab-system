7483d422a54e33a956b57c4ebfc71998c08c952a
Joel <joelkith@gmail.com>
2018-06-11 18:23:18 +0300

    PDF tests

--- app/Http/Controllers/ViralbatchController.php
+++ app/Http/Controllers/ViralbatchController.php
@@ -580,7 +580,7 @@ class ViralbatchController extends Controller
         
         $data = Lookup::get_viral_lookups();
         $data['batches'] = $batches;
-        $mpdf = new Mpdf('c', 'A4-L');
+        $mpdf = new Mpdf(['format' => 'A4-L']);
         $view_data = view('exports.mpdf_viralsamples_summary', $data)->render();
         $mpdf->WriteHTML($view_data);
         $mpdf->Output('summary.pdf', \Mpdf\Output\Destination::DOWNLOAD);
