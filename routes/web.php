a2d3feb18b21e3416e8dde1255484aeee3836066
Joel <joelkith@gmail.com>
2018-06-21 14:35:32 +0300

    Batch Searches.

--- routes/web.php
+++ routes/web.php
@@ -65,7 +65,7 @@ Route::middleware(['web', 'auth'])->group(function(){
 		// Route::get('index/{batch_complete?}/{page?}/{date_start?}/{date_end?}', 'BatchController@index');
 		Route::get('index/{batch_complete?}/{date_start?}/{date_end?}/{facility_id?}/{subcounty_id?}/{partner_id?}', 'BatchController@index');
 		Route::get('facility/{facility_id}/{batch_complete?}/{date_start?}/{date_end?}', 'BatchController@facility_batches');
-		Route::post('index', 'BatchController@batch_search')
+		Route::post('index', 'BatchController@batch_search');
 		Route::get('dispatch/', 'BatchController@batch_dispatch');
 		Route::post('complete_dispatch/', 'BatchController@confirm_dispatch');
 		Route::get('site_approval/', 'BatchController@approve_site_entry');
