cf3e678ad2a0a7bca4730daae4e011fc42aadf54
Joel <joelkith@gmail.com>
2018-07-04 12:12:53 +0300

    DR

--- app/Http/Controllers/ViralworksheetController.php
+++ app/Http/Controllers/ViralworksheetController.php
@@ -75,7 +75,7 @@ class ViralworksheetController extends Controller
                 ->join('viralbatches', 'viralsamples.batch_id', '=', 'viralbatches.id')
                 ->leftJoin('users', 'users.id', '=', 'viralbatches.user_id')
                 ->join('viralpatients', 'viralsamples.patient_id', '=', 'viralpatients.id')
-                ->leftJoin('facilitys', 'facilitys.id', '=', 'batches.facility_id')
+                ->leftJoin('facilitys', 'facilitys.id', '=', 'viralbatches.facility_id')
                 ->whereYear('datereceived', '>', 2014)
                 ->when(($machine_type == 1 || $machine_type == 3), function($query){
                     return $query->where('sampletype', 1);
@@ -154,7 +154,7 @@ class ViralworksheetController extends Controller
                 ->join('viralbatches', 'viralsamples.batch_id', '=', 'viralbatches.id')
                 ->leftJoin('users', 'users.id', '=', 'viralbatches.user_id')
                 ->join('viralpatients', 'viralsamples.patient_id', '=', 'viralpatients.id')
-                ->leftJoin('facilitys', 'facilitys.id', '=', 'batches.facility_id')
+                ->leftJoin('facilitys', 'facilitys.id', '=', 'viralbatches.facility_id')
                 ->whereYear('datereceived', '>', 2014)
                 ->when(($worksheet->machine_type == 1 || $worksheet->machine_type == 3), function($query){
                     return $query->where('sampletype', 1);
