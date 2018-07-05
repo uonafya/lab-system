9492cf01ae584a56e8dfbac7aef2a64acb8d5f11
Joel <joelkith@gmail.com>
2018-07-05 10:52:27 +0300

    Misc

--- resources/views/forms/upload_results.blade.php
+++ resources/views/forms/upload_results.blade.php
@@ -103,7 +103,7 @@
                                 <span class="input-group-addon btn btn-default btn-file">
                                     <span class="fileinput-new">Select Excel</span>
                                     <span class="fileinput-exists">Change</span>
-                                    <input id="upload" type="file" required accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" name="upload" >
+                                    <input id="upload" type="file" required accept=".xlsx, .xls, .csv" name="upload" >
                                 </span>
                                 <a href="#" class="input-group-addon btn btn-default fileinput-exists" data-dismiss="fileinput">Remove</a>
                             </div>
