df26ae3548c1f223e05961ed9d20136033d4431d
Joel <joelkith@gmail.com>
2018-08-10 09:23:40 +0300

    Dr Emails

--- resources/views/exports/mpdf_dr.blade.php
+++ resources/views/exports/mpdf_dr.blade.php
@@ -17,14 +17,8 @@
 
     <!-- Vendor styles -->
     <link rel="stylesheet" href="{{ asset('vendor/fontawesome/css/font-awesome.css') }}" />
-    <link rel="stylesheet" href="{{ asset('vendor/metisMenu/dist/metisMenu.css') }}" />
     <link rel="stylesheet" href="{{ asset('vendor/animate.css/animate.css') }}" />
     <link rel="stylesheet" href="{{ asset('vendor/bootstrap/dist/css/bootstrap.css') }}" />
-
-    <!-- App styles -->
-    <link rel="stylesheet" href="{{ asset('fonts/pe-icon-7-stroke/css/pe-icon-7-stroke.css') }}" />
-    <link rel="stylesheet" href="{{ asset('fonts/pe-icon-7-stroke/css/helper.css') }}" />
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
 
     <style type="text/css">
         body.light-skin #menu {
@@ -169,15 +163,7 @@
 
 <script src="{{ asset('vendor/jquery/dist/jquery.min.js') }}"></script>
 <script src="{{ asset('vendor/jquery-ui/jquery-ui.min.js') }}"></script>
-<script src="{{ asset('vendor/slimScroll/jquery.slimscroll.min.js') }}"></script>
 <script src="{{ asset('vendor/bootstrap/dist/js/bootstrap.min.js') }}"></script>
-
-<script src="{{ asset('vendor/metisMenu/dist/metisMenu.min.js') }}"></script>
-<script src="{{ asset('vendor/iCheck/icheck.min.js') }}"></script>
-<script src="{{ asset('vendor/peity/jquery.peity.min.js') }}"></script>
-<script src="{{ asset('vendor/sparkline/index.js') }}"></script>
-<script src="{{ asset('js/homer.js') }}"></script>
-
 
 </body>
 </html>
