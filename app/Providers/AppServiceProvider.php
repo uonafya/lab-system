e19b525c3993a2e7897daaa890a3f7a0b3c1ad08
Joel <joelkith@gmail.com>
2018-08-15 16:49:24 +0300

    Https fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -39,6 +39,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        \Illuminate\Support\Facades\URL::forceScheme('https');
+        
         Mother::observe(MotherObserver::class);
 
         Batch::observe(BatchObserver::class);
