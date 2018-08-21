d3688d6b63437c0fe14824f64f0411717cd3d957
Joel <joelkith@gmail.com>
2018-08-21 13:07:46 +0300

    Form changes

--- app/MiscDr.php
+++ app/MiscDr.php
@@ -5,28 +5,90 @@ namespace App;
 use GuzzleHttp\Client;
 
 use App\Common;
+use App\DrSample;
 
 class MiscDr extends Common
 {
 
 	public static function create_plate($worksheet)
 	{
-		$client = new Client(['base_uri' => self::$base]);
+		$client = new Client(['base_uri' => 'https://blablabla']);
 
 		$path = storage_path('app/public/results/dr/' . $worksheet->id . '/');
 
-		
+		$samples = $worksheet->sample;
+		$samples->load(['result']);
 
-		$response = $client->request('post', 'insert/patients', [
+		$primers = ['F1', 'F2', 'F3', 'R1', 'R2', 'R3'];
+
+		$sample_data = [];
+
+		foreach ($samples as $key => $sample) {
+			$s = [
+				'type' => 'sample_create',
+				'attributes' => [
+					'sample_name' => "{$sample->id}",
+					'pathogen' => 'hiv',
+					'assay' => 'cdc-hiv',
+					'enforce_recall' => false,
+					'sample_type' => 'data',
+				],
+			];
+
+			$abs = [];
+
+			foreach ($primers as $primer) {
+				$abs[] = self::find_ab_file($path, $sample, $primer);
+			}
+			$s['attributes']['ab1s'] = $abs;
+			$sample_data[] = $s;
+		}
+
+		$response = $client->request('post', '', [
 			'headers' => [
 				'Accept' => 'application/json',
 				'X-Hyrax-Apikey' => env('DR_KEY'),
 			],
 			'json' => [
-				'patients' => $patients->toJson(),
-				'lab_id' => env('APP_LAB', null),
+				[
+					'type' => 'plate_create',
+					'attributes' => [
+						'plate_name' => "{$worksheet->id}",
+					],
+				],
+				'included' => $sample_data,
 			],
-
 		]);
 	}
+
+	public static function find_ab_file($path, $obj, $primer)
+	{
+		$files = scandir($path);
+		if(!$files) return null;
+
+		foreach ($files as $file) {
+			if($file == '.' || $file == '..') continue;
+
+			$new_path = $path . $file;
+			if(is_dir($new_path)){
+				$a = self::find_ab_file($new_path, $obj, $primer);
+
+				if(!$a) continue;
+				return $a;
+			}
+			else{
+				if(starts_with($file, $obj->id . $primer)){
+					$a = [
+						'filename' => $file,
+						'data' => base64_encode(file_get_contents($new_path)),
+					];
+					return $a;
+				}
+				continue;
+			}
+		}
+		return false;
+	}
+
+
 }
