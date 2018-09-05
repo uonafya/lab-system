532e6e3d6d394a6f83ad9057cfee68cac357df97
Joel <joelkith@gmail.com>
2018-09-05 12:40:30 +0300

    Fixes

--- resources/views/shared/samples-partial.blade.php
+++ resources/views/shared/samples-partial.blade.php
@@ -1,54 +1,60 @@
-<table  class="table table-striped table-bordered table-hover data-table">
-	<thead>
-		<tr>
-			<th>#</th>
-			<th>Lab ID</th>
-			<th>Patient</th>
-			<th>Facility</th>
-			<th>Entry Type</th>
-			<th>Spots</th>
-			<th>Run</th>
-			<th>Date Collected</th>
-			<th>Entered By</th>
-			<th>Release as Redraw</th>
-			<th>Update</th>
-			<th>Delete</th>
-		</tr>
-	</thead>
-	<tbody>
-		@foreach($samples as $key => $sample)
+<div class="table-responsive">
+	<table  class="table table-striped table-bordered table-hover data-table">
+		<thead>
 			<tr>
-				<td> {{ ($key+1) }} </td>
-				<td> {{ $sample->id }} </td>
-				<td> {{ $sample->patient }} </td>
-				<td> {{ $sample->name }} </td>
-				@if($sample->site_entry == 0)
-					<td> Lab Entry </td>
-				@elseif($sample->site_entry == 1)
-					<td> Site Entry </td>
-				@endif
-				<td> {{ $sample->spots }} </td>
-				<td> {{ $sample->run }} </td>
-				<td> {{ $sample->datecollected }} </td>
-
-				@if($sample->site_entry == 0)
-					<td> {{ $sample->surname . ' ' . $sample->oname }} </td>
-				@elseif($sample->site_entry == 1)
-					<td>  </td>
-				@endif
-
-
-                <td> <a href="{{ url('sample/release/' . $sample->id) }}" class="confirmAction"> Release</a> </td>
-                <td> <a href="{{ url('sample/' . $sample->id . '/edit') }}"> Edit</a> </td>
-                <td> 
-                    {{ Form::open(['url' => 'sample/' . $sample->id, 'method' => 'delete', 'onSubmit' => "return confirm('Are you sure you want to delete the following sample?');"]) }}
-                        <button type="submit" class="btn btn-xs btn-primary">Delete</button>
-                    {{ Form::close() }} 
-                    
-                </td>
+				<th>#</th>
+				<th>Lab ID</th>
+				<th>Patient</th>
+				<th>Facility</th>
+				<th>Entry Type</th>
+				<th>Spots</th>
+				<th>Run</th>
+				<th>Previous Runs</th>
+				<th>Original ID</th>
+				<th>Date Collected</th>
+				<th>Entered By</th>
+				<th>Release as Redraw</th>
+				<th>Update</th>
+				<th>Delete</th>
 			</tr>
-		@endforeach
+		</thead>
+		<tbody>
+			@foreach($samples as $key => $sample)
+				<tr>
+					<td> {{ ($key+1) }} </td>
+					<td> {{ $sample->id }} </td>
+					<td> {{ $sample->patient }} </td>
+					<td> {{ $sample->name }} </td>
+					@if($sample->site_entry == 0)
+						<td> Lab Entry </td>
+					@elseif($sample->site_entry == 1)
+						<td> Site Entry </td>
+					@endif
+					<td> {{ $sample->spots }} </td>
+					<td> {{ $sample->run }} </td>
+					<td><a href="{{ url('sample/runs/' . $sample->id) }}"> Runs</a> </td>
+					<td> {{ $sample->parentid ?? null }} </td>
+					<td> {{ $sample->datecollected }} </td>
 
-	</tbody>
+					@if($sample->site_entry == 0)
+						<td> {{ $sample->surname . ' ' . $sample->oname }} </td>
+					@elseif($sample->site_entry == 1)
+						<td>  </td>
+					@endif
 
-</table>
+
+	                <td> <a href="{{ url('sample/release/' . $sample->id) }}" class="confirmAction"> Release</a> </td>
+	                <td> <a href="{{ url('sample/' . $sample->id . '/edit') }}"> Edit</a> </td>
+	                <td> 
+	                    {{ Form::open(['url' => 'sample/' . $sample->id, 'method' => 'delete', 'onSubmit' => "return confirm('Are you sure you want to delete the following sample?');"]) }}
+	                        <button type="submit" class="btn btn-xs btn-primary">Delete</button>
+	                    {{ Form::close() }} 
+	                    
+	                </td>
+				</tr>
+			@endforeach
+
+		</tbody>
+
+	</table>
+</div>
