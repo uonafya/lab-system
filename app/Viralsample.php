270b092d918fc04bd82710973e60baf4ca16e74a
Joel <joelkith@gmail.com>
2018-09-06 11:42:57 +0300

    Fixes

--- app/Viralsample.php
+++ app/Viralsample.php
@@ -103,8 +103,8 @@ class Viralsample extends BaseModel
     {
         if($this->sampletype == 1) return "PLASMA";
         else if($this->sampletype == 2) return "EDTA";
-        else if($this->sampletype == 3) return "DBS Venous";
-        else if($this->sampletype == 4) return "DBS Capillary";
+        else if($this->sampletype == 3) return "DBS Capillary";
+        else if($this->sampletype == 4) return "DBS Venous";
         return "";
     }
 
