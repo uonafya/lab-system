66f13d6ab4361c7695cd9ffdbf4e39c7d762ced5
Joel <joelkith@gmail.com>
2018-09-11 00:10:09 +0300

    Synch Changes

--- app/Synch.php
+++ app/Synch.php
@@ -245,6 +245,8 @@ class Synch
 
 			$body = json_decode($response->getBody());
 
+			dd($body);
+
 			foreach ($body->batches as $key => $value) {
 				$update_data = ['national_batch_id' => $value->national_batch_id, 'synched' => 1, 'datesynched' => $today,];
 				$batch_class::where('id', $value->original_id)->update($update_data);
