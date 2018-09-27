fbbcee40583ae22b583c1af44bea69406b9172bd
Joel <joelkith@gmail.com>
2018-09-27 21:54:58 +0300

    API Changes

--- app/Api/V1/Requests/EidCompleteRequest.php
+++ app/Api/V1/Requests/EidCompleteRequest.php
@@ -17,6 +17,7 @@ class EidCompleteRequest extends BaseRequest
         $semi = array_merge($base, $eid);
         $val = array_merge($semi, $complete);
         $val['dob'] = array_merge($val['dob'], [new BeforeOrEqual($this->input('datecollected'), 'datecollected')]);
+        $val['datecollected'] = array_merge($val['datecollected'], [new BeforeOrEqual($this->input('datereceived'), 'datereceived')]);
         $val['datereceived'] = array_merge($val['datereceived'], [new BeforeOrEqual($this->input('datetested'), 'datetested')]);
         $val['datetested'] = array_merge($val['datetested'], [new BeforeOrEqual($this->input('datedispatched'), 'datedispatched')]);
         
