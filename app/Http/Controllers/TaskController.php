f7c40ca0d3652f8c65e46da4e90fe5e2223b6c9e
Unknown <baksajoshua09@gmail.com>
2018-10-02 11:32:52 +0300

    Allow 0 for kit deliveries

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -56,12 +56,12 @@ class TaskController extends Controller
         $abbottdeliveries = Abbotdeliveries::selectRaw("count(*) as entries")->where('year', '=', date('Y'))->where('quarter', parent::_getMonthQuarter(date('m')))->first()->entries;
 
         if ($request->saveTaqman) {
-            $receivedby = $request->receivedby;
-            $datereceived = $request->datereceived;
-            $vreceivedby = $request->vreceivedby;
-            $vdatereceived = $request->vdatereceived;
+            $receivedby = $request->receivedby ?? NULL;
+            $datereceived = $request->datereceived ?? NULL;
+            $vreceivedby = $request->vreceivedby ?? NULL;
+            $vdatereceived = $request->vdatereceived ?? NULL;
             $lab = auth()->user()->lab_id;
-            $quarter = $request->quarter;
+            $quarter = $request->quarter ?? NULL;
             $status = 1;
             $source = 3;
             $year = date('Y');
@@ -100,10 +100,10 @@ class TaskController extends Controller
                 return redirect()->route('pending');
         } else if ($request->saveAbbott) {
             // dd($request->all());
-            $receivedby = $request->areceivedby;
-            $datereceived = $request->adatereceived;
-            $vreceivedby = $request->vareceivedby;
-            $vdatereceived = $request->vadatereceived;
+            $receivedby = $request->areceivedby ?? NULL;
+            $datereceived = $request->adatereceived ?? NULL;
+            $vreceivedby = $request->vareceivedby ?? NULL;
+            $vdatereceived = $request->vadatereceived ?? NULL;
             $lab = auth()->user()->lab_id;
             $quarter = $request->quarter;
             $status = 1;
