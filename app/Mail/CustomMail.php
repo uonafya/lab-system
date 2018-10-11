79ed7337998815216c9493f986be3951fb5dcc1a
Joel <joelkith@gmail.com>
2018-10-11 18:38:34 +0300

    Updates

---
+++ app/Mail/CustomMail.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+use \App\Lab;
+
+class CustomMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $email;
+    public $facility;
+    public $lab;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($email, $facility=null)
+    {
+        $this->email = $email;
+        $this->facility = $facility;
+        $this->lab = Lab::find(env('APP_LAB'));
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        $view_name = 'emails.' . $email->id;
+        $from = $this->email->from_name ?? env('MAIL_FROM_NAME');
+        return $this->subject($this->email->subject)->from(env('MAIL_FROM_ADDRESS'), $from)->view($view_name);
+    }
+}
---
+++ app/OldModels/PocWorklist.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\OldModels;
+
+use App\OldModels\BaseModel;
+
+class PocWorklist extends BaseModel
+{
+	protected $table = 'pocworklists';
+}
