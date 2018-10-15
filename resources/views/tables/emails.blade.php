df70cce239558daac12eea016f767147e8ea1b01
Joel <joelkith@gmail.com>
2018-10-15 14:42:50 +0300

    Send Emails

--- resources/views/tables/emails.blade.php
+++ resources/views/tables/emails.blade.php
@@ -21,6 +21,7 @@
                                     <th>BCC List</th>
                                     <th>Lab Signature</th>
                                     <th>Time To Be Sent</th>
+                                    <th>Sent</th>
                                     <th>Test Email</th>
                                     <th>Edit Email</th>
                                     <th>Delete Email</th>
@@ -36,6 +37,11 @@
                                         <td> {{ $email->cc_list ?? '' }} </td>
                                         <td> {{ $email->bcc_list ?? '' }} </td>
                                         <td> {{ $email->lab_signature ?? '' }} </td>
+                                        @if($email->sent)
+                                            <td>True</td>
+                                        @else
+                                            <td>False</td>
+                                        @endif
                                         <td> {{ $email->my_date_format('time_to_be_sent', 'd/m/Y h:i:s a') }} </td>
                                         <td>
                                             <a href="{{ url('/email/preview/' . $email->id ) }} " target='_blank'>Preview</a>
