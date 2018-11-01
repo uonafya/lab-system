81c479d7df205b4143470f0ff818e91f5789ed62
Joel <joelkith@gmail.com>
2018-11-01 21:19:18 +0300

    Updates

--- resources/views/exports/mpdf_dr.blade.php
+++ resources/views/exports/mpdf_dr.blade.php
@@ -75,22 +75,22 @@
 
    
                         <div class="form-group">
-                            <label class="col-sm-4 control-label">Facility 
+                            <label class="col-sm-3 control-label">Facility 
                                 <strong><div style='color: #ff0000; display: inline;'>*</div></strong>
                             </label>
 
-                            <div class="col-sm-8">
-                                <input class="form-control requirable" required name="patient" type="text" value="
+                            <div class="col-sm-9">
+                                <input class="form-control requirable" required name="facility" type="text" value="
                                 {{ $sample->patient->facility->facilitycode }} {{ $sample->patient->facility->name }}
-                                " id="patient">
+                                " id="facility">
                             </div>
                         </div>
 
                         <div class="form-group">
-                            <label class="col-sm-4 control-label">Patient / Sample ID
+                            <label class="col-sm-3 control-label">Patient / Sample ID
                                 <strong><div style='color: #ff0000; display: inline;'>*</div></strong>
                             </label>
-                            <div class="col-sm-8">
+                            <div class="col-sm-9">
                                 <input class="form-control requirable" required name="patient" type="text" value="{{ $sample->patient->patient ?? '' }}" id="patient">
                             </div>
                         </div>
@@ -98,26 +98,26 @@
                         <div class="hr-line-dashed"></div>
 
                         <div class="form-group">
-                            <label class="col-sm-4 control-label">Reason for DR test
+                            <label class="col-sm-3 control-label">Reason for DR test
                                 <strong><div style='color: #ff0000; display: inline;'>*</div></strong>
                             </label>
-                            <div class="col-sm-8">
+                            <div class="col-sm-9">
                                 <input class="form-control requirable" required name="dr_reasons" type="text" value="{{ $drug_resistance_reasons->where('id', $sample->dr_reason_id)->first()->name ?? '' }}" id="dr_reasons">
                             </div>
                         </div>
 
                         <div class="form-group">
-                            <label class="col-sm-4 control-label">Previous Regimen</label>
-                            <div class="col-sm-8">
+                            <label class="col-sm-3 control-label">Previous Regimen</label>
+                            <div class="col-sm-9">
                                 <input class="form-control" name="prev_prophylaxis" type="text" value="" id="prev_prophylaxis">
                             </div>
                         </div>
 
                         <div class="form-group">
-                            <label class="col-sm-4 control-label">Current Regimen
+                            <label class="col-sm-3 control-label">Current Regimen
                                 <strong><div style='color: #ff0000; display: inline;'>*</div></strong>
                             </label>
-                            <div class="col-sm-8">
+                            <div class="col-sm-9">
                                 <input class="form-control requirable" required name="prophylaxis" type="text" value="" id="prophylaxis">
                             </div>
                         </div>
@@ -125,24 +125,194 @@
                         <div class="hr-line-dashed"></div>                        
 
                         <div class="form-group">
-                            <label class="col-sm-4 control-label">Date Started on Previous Regimen</label>
-                            <div class="col-sm-8">
+                            <label class="col-sm-3 control-label">Date Started on Previous Regimen</label>
+                            <div class="col-sm-9">
                                 <input class="form-control" name="date_prev_regimen" type="text" value="" id="date_prev_regimen">
                             </div>                          
                         </div>                      
 
                         <div class="form-group">
-                            <label class="col-sm-4 control-label">Date Started on Current Regimen</label>
-                            <div class="col-sm-8">
+                            <label class="col-sm-3 control-label">Date Started on Current Regimen</label>
+                            <div class="col-sm-9">
                                 <input class="form-control" name="date_current_regimen" type="text" value="" id="date_current_regimen">
                             </div>                          
                         </div>
 
                         <div class="form-group">
-                            <label class="col-sm-4 control-label">Date of Collection
+                            <label class="col-sm-3 control-label">Clinical Indication
                                 <strong><div style='color: #ff0000; display: inline;'>*</div></strong>
                             </label>
-                            <div class="col-sm-8">
+                            <div class="col-sm-9">
+                                @foreach ($clinical_indications as $clinical_indication)
+                                    <div>
+                                        <label> 
+                                            <input name="clinical_indications[]" type="checkbox" class="i-checks" required
+                                                value="{{ $clinical_indication->id }}" 
+
+                                                @if(isset($sample) && is_array($sample->clinical_indications_array) &&
+                                                in_array($clinical_indication->id, $sample->clinical_indications_array) )
+                                                    checked="checked"
+                                                @endif
+                                            /> 
+                                            {{ $clinical_indication->name }} 
+                                        </label>
+                                    </div>
+
+                                @endforeach
+                            </div>
+                        </div>                     
+
+                        <div class="form-group">
+                            <label class="col-sm-3 control-label">Opportunistic Infections (If Any)</label>
+                            <div class="col-sm-9">
+                                <input class="form-control" name="date_current_regimen" type="text" value="" id="date_current_regimen">
+                            </div>                          
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-sm-3">
+                                <label><input type="checkbox" class="i-checks" name="has_opportunistic_infections" value={{ $key }} 
+
+                                    @if(isset($sample) && $sample->has_tb)
+                                        checked="checked"
+                                    @endif
+                                    > 
+                                    Has TB
+                                </label>
+                            </div>
+                            <label class="col-sm-3 control-label">Treatment Phase</label>
+                            <div class="col-sm-6">
+                                @foreach ($tb_treatment_phases as $tb_treatment_phase)
+                                    <div>
+                                        <label> 
+                                            <input name="clinical_indications[]" type="checkbox" class="i-checks" required /> 
+                                            {{ $tb_treatment_phase->name }} 
+                                        </label>
+                                    </div>
+
+                                @endforeach                                
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-sm-3">
+                                <label><input type="checkbox" class="i-checks" name="has_opportunistic_infections" value={{ $key }} 
+
+                                    @if(isset($sample) && $sample->has_tb)
+                                        checked="checked"
+                                    @endif
+                                    > 
+                                    Has ARV Toxicity
+                                </label>
+                            </div>
+                            <label class="col-sm-3 control-label">ARV Toxicities</label>
+                            <div class="col-sm-6">
+                                @foreach ($arv_toxicities as $arv_toxicity)
+                                    <div>
+                                        <label> 
+                                            <input name="clinical_indications[]" type="checkbox" class="i-checks" required /> 
+                                            {{ $arv_toxicity->name }} 
+                                        </label>
+                                    </div>
+
+                                @endforeach                                
+                            </div>
+                        </div>                     
+
+                        <div class="form-group">
+                            <label class="col-sm-3 control-label">CD4 result with the last 6 Months</label>
+                            <div class="col-sm-9">
+                                <input class="form-control" name="cd4_result" type="text" value="{{ $sample->cd4_result ?? '' }}" id="cd4_result">
+                            </div>
+                        </div>
+
+
+                        <div class="form-group">
+                            <div class="col-sm-3">
+                                <label><input type="checkbox" class="i-checks" name="has_opportunistic_infections" value={{ $key }} 
+
+                                    @if(isset($sample) && $sample->has_tb)
+                                        checked="checked"
+                                    @endif
+                                    > 
+                                    Has Missed Pills
+                                </label>
+                            </div>
+                            <label class="col-sm-3 control-label">No of Missed Pills</label>
+                            <div class="col-sm-6">
+                                <input class="form-control" name="datecollected" type="text" value="" id="datecollected">          
+                            </div>
+                        </div> 
+
+
+                        <div class="form-group">
+                            <div class="col-sm-3">
+                                <label><input type="checkbox" class="i-checks" name="has_opportunistic_infections" value={{ $key }} 
+
+                                    @if(isset($sample) && $sample->has_tb)
+                                        checked="checked"
+                                    @endif
+                                    > 
+                                    Has Missed Visits
+                                </label>
+                            </div>
+                            <label class="col-sm-3 control-label">No of Missed Visits</label>
+                            <div class="col-sm-6">
+                                <input class="form-control" name="datecollected" type="text" value="" id="datecollected">          
+                            </div>
+                        </div> 
+
+
+                        <div class="form-group">
+                            <div class="col-sm-6">
+                                <label><input type="checkbox" class="i-checks" name="has_opportunistic_infections" value={{ $key }} 
+
+                                    @if(isset($sample) && $sample->has_tb)
+                                        checked="checked"
+                                    @endif
+                                    > 
+                                    Has Missed Pills Because of Missed Visits
+                                </label>
+                            </div>
+                        </div> 
+
+                        <div class="form-group"  >
+                            <label class="col-sm-3 control-label">Other Medications </label>
+                            <div class="col-sm-9">
+                                @foreach ($other_medications as $other_medication)
+                                    <div>
+                                        <label> 
+                                            <input name="other_medications[]" type="checkbox" class="i-checks"
+                                                value="{{ $other_medication->id }}" 
+
+                                                @if(isset($sample) && is_array($sample->other_medications_array) &&
+                                                in_array($other_medication->id, $sample->other_medications_array) )
+                                                    checked="checked"
+                                                @endif
+                                            /> 
+                                            {{ $other_medication->name }} 
+                                        </label>
+                                    </div>
+
+                                @endforeach
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label class="col-sm-3 control-label">Other Medications (Separate using commas) </label>
+                            <div class="col-sm-9">
+                                <input class="form-control" name="other_medications_text" type="text" value="{{ $sample->other_medications_string ?? '' }}" id="other_medications_text">
+                            </div>
+                        </div>
+
+
+
+
+                        <div class="form-group">
+                            <label class="col-sm-3 control-label">Date of Collection
+                                <strong><div style='color: #ff0000; display: inline;'>*</div></strong>
+                            </label>
+                            <div class="col-sm-9">
                                 <input class="form-control" name="datecollected" type="text" value="" id="datecollected">
                             </div>                           
                         </div>
@@ -164,6 +334,7 @@
 <script src="{{ asset('vendor/jquery/dist/jquery.min.js') }}"></script>
 <script src="{{ asset('vendor/jquery-ui/jquery-ui.min.js') }}"></script>
 <script src="{{ asset('vendor/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('vendor/iCheck/icheck.min.js') }}"></script>
 
 </body>
 </html>
