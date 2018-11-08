ba2effc54362797354743399e1d6acc5ae3d33b3
Joel <joelkith@gmail.com>
2018-11-08 13:14:00 +0300

    Updates

--- resources/views/forms/email.blade.php
+++ resources/views/forms/email.blade.php
@@ -42,6 +42,7 @@
                                 <select class="form-control" name="county_id">
 
                                   <option></option>
+                                  <option value="">All Counties</option>
                                   @foreach ($counties as $county)
                                       <option value="{{ $county->id }}"
 
