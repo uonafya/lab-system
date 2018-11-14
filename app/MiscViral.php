626d18dff9d26f87eee47a28706c236e17633d21
Joel <joelkith@gmail.com>
2018-11-14 10:33:36 +0300

    Updates

--- app/MiscViral.php
+++ app/MiscViral.php
@@ -875,7 +875,7 @@ class MiscViral extends Common
         })->download('csv');
     }
 
-    public static function find_no_reruns()
+    public static function find_no_result()
     {
         ini_set("memory_limit", "-1");
         $samples = Viralsample::whereNotNull('interpretation')->whereRaw("(result is null or result = '')")->get();
@@ -906,9 +906,61 @@ class MiscViral extends Common
                     self::save_repeat($sample->id);   
                 }
             }
+        }
+    } 
 
+    public static function find_no_result()
+    {
+        ini_set("memory_limit", "-1");
+        $samples = Viralsample::whereNotNull('interpretation')->whereRaw("(result is null or result = '')")->get();
 
+        $cutoff = Carbon::parse('2018-11-01');
 
+        foreach ($samples as $sample) {
+            $data = self::sample_result($sample->interpretation);
+            $sample->fill($data);
+            if($sample->result != '' || $sample->result != 'Failed' || $sample->result != 'Collect New Sample'){
+                $sample->repeatt = 0;
+                $sample->save();
+            }
+            else{
+                $datetested = Carbon::parse($sample->datetested);
+
+                if($cutoff->greaterThan($datetested)){
+
+                    $sample->result = "Collect New Sample";
+                    $sample->labcomment = "Failed Test";
+                    $sample->repeatt = 0;
+                    $sample->save();
+
+                }
+                else{
+                    $sample->repeatt = 1;
+                    $sample->save();
+                    self::save_repeat($sample->id);   
+                }
+            }
+        }
+    } 
+
+    public static function find_no_reruns()
+    {
+        ini_set("memory_limit", "-1");
+        $samples = ViralsampleView::where(['repeatt' => 1])->whereNull('datedispatched')
+                        ->whereBetween('datetested', ['2018-01-01', '2018-10-31'])
+                        ->get();
+
+        foreach ($samples as $s) {
+
+            $sample = Viralsample::find($s->id);
+            if($sample->has_rerun) continue;
+
+            return $sample->interpretation;
+
+            // $sample->result = "Collect New Sample";
+            // $sample->labcomment = "Failed Test";
+            // $sample->repeatt = 0;
+            // $sample->save();
         }
     } 
     
