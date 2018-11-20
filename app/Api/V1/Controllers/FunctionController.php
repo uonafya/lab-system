9316d4ba0d1d3931056175f46a9658386ca89e43
Joel <joelkith@gmail.com>
2018-11-20 11:14:18 +0300

    Updates

--- app/Api/V1/Controllers/FunctionController.php
+++ app/Api/V1/Controllers/FunctionController.php
@@ -198,7 +198,7 @@ class FunctionController extends Controller
                 if($test < 3) return $query->where(['repeatt' => 0]);
             })            
             ->orderBy('created_at', 'desc')
-            ->paginate(20);
+            ->paginate(50);
 
         $result->transform(function ($sample, $key) use ($test){
             // return ['patient age' => $item->age];
