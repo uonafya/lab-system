4994f1ea4d629985917a1d04b370bfa25c18a71a
Unknown <baksajoshua09@gmail.com>
2018-11-21 11:41:55 +0300

    Patient Name search

--- app/Http/Controllers/Cd4PatientController.php
+++ app/Http/Controllers/Cd4PatientController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Cd4Patient;
+use App\Cd4Sample;
+use App\Lookup;
 use Illuminate\Http\Request;
 
 class Cd4PatientController extends Controller
@@ -83,20 +85,44 @@ class Cd4PatientController extends Controller
         //
     }
 
-    public function search_name(Request $request) {
-        $search = $request->input('search');
-        $patients = Cd4Patient::where('patient_name', 'like', '%'.$search.'%')->paginate(10);
+    public function search_name(Request $request, $recordno=null) {
+        if($request->method() == "POST"){
+            $search = $request->input('search');
+            $patients = Cd4Patient::where('patient_name', 'like', '%'.$search.'%')->paginate(10);
+            
+            $patients->setPath(url()->current());
+            return $patients;
+        } else {
+            $patient = Cd4Patient::where('medicalrecordno', '=', $recordno)->first();
+            $samples = Cd4Sample::where('patient_id', '=', $patient->id)->orderBy('datecollected', 'desc')->get();
+
+            $data = Lookup::cd4_lookups();
+            $data['samples'] = $samples;
+            $data = (object) $data;
+            // dd($data);
+            return view('tables.cd4-samples', compact('data'))->with('pageTitle', 'Samples Summary');
+        }
         
-        $patients->setPath(url()->current());
-        return $patients;
     }
 
-    public function search_record_no(Request $request) {
-        $search = $request->input('search');
-        $patients = Cd4Patient::where('medicalrecordno', 'like', '%'.$search.'%')->paginate(10);
+    public function search_record_no(Request $request, $recordno=null) {
+        if($request->method() == "POST"){
+            $search = $request->input('search');
+            $patients = Cd4Patient::where('medicalrecordno', 'like', '%'.$search.'%')->paginate(10);
+            
+            $patients->setPath(url()->current());
+            return $patients;
+        } else {
+            $patient = Cd4Patient::where('medicalrecordno', '=', $recordno)->first();
+            $samples = Cd4Sample::where('patient_id', '=', $patient->id)->orderBy('datecollected', 'desc')->get();
+
+            $data = Lookup::cd4_lookups();
+            $data['samples'] = $samples;
+            $data = (object) $data;
+            // dd($data);
+            return view('tables.cd4-samples', compact('data'))->with('pageTitle', 'Samples Summary');
+        }
         
-        $patients->setPath(url()->current());
-        return $patients;
     }
 
     public function new_patient(Request $request)
