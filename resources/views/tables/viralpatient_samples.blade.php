107464834fc7c1b5b4c8f81b73bfdcb24be143a0
Joel <joelkith@gmail.com>
2018-11-21 10:30:07 +0300

    Updates

--- resources/views/tables/viralpatient_samples.blade.php
+++ resources/views/tables/viralpatient_samples.blade.php
@@ -35,7 +35,7 @@
                                         <td> {{ $key+1 }} </td>
                                         <td> {{ $patient->patient ?? '' }} </td>
                                         <td> {{ $patient->facility->name ?? '' }} </td>
-                                        <td>  {!! $sample->batch->hyper_link !!} </td>
+                                        <td>  {!! $sample->batch->hyper_link ?? '' !!} </td>
                                         <td>
                                             @foreach($received_statuses as $received_status)
                                                 @if($sample->receivedstatus == $received_status->id)
