1af19a425e4cc9f669fe506d12059d1162e3c519
Joel <joelkith@gmail.com>
2018-11-22 10:29:28 +0300

    Updates

--- app/Api/V1/Controllers/EidController.php
+++ app/Api/V1/Controllers/EidController.php
@@ -75,6 +75,7 @@ class EidController extends BaseController
         $batch->facility_id = $facility;
         $batch->datereceived = $datereceived;
         $batch->user_id = 0;
+        if(env('APP_LAB') == 5) $batch->user_id = 66;
         $batch->site_entry = 1;
         $batch->save();
 
