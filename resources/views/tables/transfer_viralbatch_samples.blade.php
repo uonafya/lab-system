446506ede6939aa790cde308afa9c0afdf8f9451
Joel <joelkith@gmail.com>
2018-11-28 12:56:36 +0300

    Updates

--- resources/views/tables/transfer_viralbatch_samples.blade.php
+++ resources/views/tables/transfer_viralbatch_samples.blade.php
@@ -115,6 +115,7 @@
                                 </thead>
                                 <tbody> 
                                     @foreach($samples as $key => $sample)
+                                        @continue($sample->repeatt == 1)
                                         <tr>                                           
                                             <td>
                                                 <div align='center'>
