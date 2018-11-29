ac6bff0ce34c10a7d89e6828355e8c741cc21d37
Unknown <baksajoshua09@gmail.com>
2018-11-29 11:43:49 +0300

    Auto stash before merge of "develop" and "joel/master"

---
+++ resources/views/reports/labtrackers.blade.php
@@ -0,0 +1,96 @@
+@extends('layouts.master')
+
+@section('css_scripts')
+    
+@endsection
+
+@section('custom_css')
+	<style type="text/css">
+		.input-edit {
+            background-color: #FFFFCC;
+        }
+	</style>
+@endsection
+
+@section('content')
+@php
+    $prevmonth = date('m')-1;
+@endphp
+<div class="row">
+    <div class="col-md-12">
+        <div class="hpanel" style="margin-top: 1em;margin-right: 18%;">
+        @foreach($data->sampletypes as $sampletype)
+            <div class="alert alert-warning">
+                <center>
+                    <font color="#4183D7">
+                    @if ($sampletype == 'EID')
+                        {{ $sampletype }}
+                    @else
+                        VL ({{ $sampletype }})
+                    @endif
+                    </font>
+                </center>
+            </div>
+            <div class="panel-body" style="padding: 20px;box-shadow: none; border-radius: 0px;">
+                <table class="table table-striped table-bordered table-hover data-table" style="font-size: 10px;margin-top: 1em;">
+                    <thead>
+                        <tr>
+                            <th>Period</th>
+                            <th>#Received Samples</th>
+                            <th>#Rejected Samples</th>
+                            <th># Logged in System</th>
+                            <th># NOT Logged in System</th>
+                            <th># Tested</th>
+                            <th>Reasons for Backlog</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th>
+                                {{ date("F", mktime(null, null, null, $prevmonth)) }}, {{ date('Y') }}
+                            </th>
+                            <td>
+                                <input class="form-control input-sm" id="{{ $sampletype }}received" name="{{ $sampletype }}received" type="text" value="{{ $data->logs->$sampletype->received }}" disabled="true">
+                                <input name="{{ $sampletype }}received" type="hidden" value="{{ $data->logs->$sampletype->received }}">
+                            </td>
+                            <td>
+                                <input class="form-control input-sm" id="{{ $sampletype }}rejected" name="{{ $sampletype }}rejected" type="text" value="{{ $data->logs->$sampletype->rejected }}" disabled="true">
+                                <input name="{{ $sampletype }}rejected" type="hidden" value="{{ $data->logs->$sampletype->rejected }}" >
+                            </td>
+                            <td>
+                                <input class="form-control input-sm" id="{{ $sampletype }}logged" name="{{ $sampletype }}logged" type="text" value="{{ $data->logs->$sampletype->logged }}" disabled="true">
+                                <input name="{{ $sampletype }}logged" type="hidden" value="{{ $data->logs->$sampletype->logged }}">
+                            </td>
+                            <td>
+                                <input class="form-control input-sm input-edit" id="{{ $sampletype }}notlogged" name="{{ $sampletype }}notlogged" type="text" value="" required>
+                            </td>
+                            <td>
+                                <input class="form-control input-sm" id="{{ $sampletype }}tested" name="{{ $sampletype }}tested" type="text" value="{{ $data->logs->$sampletype->tested }}" disabled="true">
+                                <input name="{{ $sampletype }}tested" type="hidden" value="{{ $data->logs->$sampletype->tested }}">
+                            </td>
+                            <td>
+                                <textarea class="form-control input-sm input-edit" id="{{ $sampletype }}reason" name="{{ $sampletype }}reason" cols="100" required></textarea>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            @endforeach
+        </div>
+    </div>
+</div>
+@endsection
+
+@section('scripts')
+    @component('/forms/scripts')
+        @slot('js_scripts')
+            
+        @endslot
+
+
+        @slot('val_rules')
+           
+        @endslot
+
+    @endcomponent
+@endsection
