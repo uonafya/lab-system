2278c62278d9ce10955ab55a6f530aca9124b8b9
Joel <joelkith@gmail.com>
2019-01-18 12:37:12 +0300

    Mlab

--- app/Http/Controllers/BatchController.php
+++ app/Http/Controllers/BatchController.php
@@ -848,6 +848,18 @@ class BatchController extends Controller
 
     }
 
+    public function convert_to_site_entry(Batch $batch)
+    {
+        if($batch->site_entry == 2 && !$batch->datedispatched){
+            $batch->site_entry = 1;
+            $batch->save();
+            session(['toast_message' => 'The batch has been converted to a site entry']);
+            return back();
+        }
+        session(['toast_message' => 'The batch has not been converted to a site entry', 'toast_error' => 1]);
+        return back();
+    }
+
 
     public function search(Request $request)
     {
