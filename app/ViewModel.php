974f30adf6cf8db5893202628a1c66d226ed0364
Joel <joelkith@gmail.com>
2019-01-18 12:05:36 +0300

    Mlab

--- app/ViewModel.php
+++ app/ViewModel.php
@@ -89,6 +89,7 @@ class ViewModel extends Model
         else{
             $a = explode('_', $attr);
             $url = url($pre . $a[0] . '/' . $this->$attr);
+            if(str_contains($c, 'patient')) $url = url($pre . $a[0] . '/' . $this->patient_id);
         }
 
         if($attr == 'id' && (!$user || ($user && $user->user_type_id == 5))) return null;
