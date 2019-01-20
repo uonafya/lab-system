dfafaed05c9e38c682b4753480354a7cff02e340
Joel <joelkith@gmail.com>
2019-01-20 21:33:12 +0300

    Synch Updates

---
+++ app/Api/V1/Controllers/BatchController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Api\V1\Controllers;
+
+use App\Batch;
+use Illuminate\Http\Request;
+
+class BatchController extends Controller
+{
+    use Dingo\Api\Routing\Helpers;
+    
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Batch  $batch
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Batch $batch)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Batch  $batch
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Batch $batch)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Batch  $batch
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Batch $batch)
+    {
+        //
+    }
+}
