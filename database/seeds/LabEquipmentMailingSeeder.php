b3619cec8001cdb094bb6c3c23c912882c6e79bd
Unknown <baksajoshua09@gmail.com>
2019-01-21 17:21:23 +0300

    Creation of the mailing list model

--- database/seeds/LabEquipmentMailingSeeder.php
+++ database/seeds/LabEquipmentMailingSeeder.php
@@ -11,6 +11,6 @@ class LabEquipmentMailingSeeder extends Seeder
      */
     public function run()
     {
-        ['njebungeibowen@gmail.com', 'mld8@cdc.gov', 'uys3@cdc.gov', 'EKirui@mgickenya.org', 'ootieno@usaid.gov', 'kouma@mgickenya.org', 'skipkerich@gmail.com', 'jbatuka@usaid.gov', 'ootieno@usaid.gov', 'jlusike@clintonhealthaccess.org', 'solwande@clintonhealthaccess.org', 'tngugi@clintonhealthaccess.org','joshua.bakasa@dataposit.co.ke','joel.kithinji@dataposit.co.ke']
+        ['njebungeibowen@gmail.com', 'mld8@cdc.gov', 'uys3@cdc.gov', 'EKirui@mgickenya.org', 'ootieno@usaid.gov', 'kouma@mgickenya.org', 'skipkerich@gmail.com', 'jbatuka@usaid.gov', 'ootieno@usaid.gov', 'jlusike@clintonhealthaccess.org', 'solwande@clintonhealthaccess.org', 'tngugi@clintonhealthaccess.org','joshua.bakasa@dataposit.co.ke','joel.kithinji@dataposit.co.ke'];
     }
 }
