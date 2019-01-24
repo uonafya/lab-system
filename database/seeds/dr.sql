fef1e3b26812e7efbc6788c29df28c22bd93b465
Joel <joelkith@gmail.com>
2019-01-24 11:05:08 +0300

    Synch Updates

--- database/seeds/dr.sql
+++ database/seeds/dr.sql
@@ -87,3 +87,81 @@ INSERT INTO `dr_warning_codes` (`id`, `error`, `name`, `description`) VALUES
 
 
 
+DROP TABLE IF EXISTS `dr_viralprophylaxis`;
+CREATE TABLE IF NOT EXISTS `dr_viralprophylaxis` (
+  `id` TINYINT unsigned NOT NULL AUTO_INCREMENT,
+  `displaylabel` varchar(50) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  'regimen1' varchar(10) NULL,
+  `regimen1_class_id` TINYINT unsigned NULL,
+  'regimen2' varchar(10) NULL,
+  `regimen2_class_id` TINYINT unsigned NULL,
+  'regimen3' varchar(10) NULL,
+  `regimen3_class_id` TINYINT unsigned NULL,
+  `description` varchar(100) NULL,
+  `line` TINYINT unsigned NOT NULL,
+  `ptype` TINYINT unsigned NOT NULL DEFAULT '2',
+  `category` TINYINT unsigned NOT NULL DEFAULT '3',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=latin1;
+
+
+INSERT INTO `dr_viralprophylaxis` (`id`, `displaylabel`, `name`, 'regimen1', `regimen1_class_id`, 'regimen2', `regimen2_class_id` 'regimen3', `regimen3_class_id`, `line`, `ptype`, `category`) VALUES
+	(1, '4 &nbsp;AZT+3TC+NVP', 'AZT+3TC+NVP', 'AZT', 12, '3TC', 10, 'NVP', 8, 0, 0, 4),
+	(2, '5 &nbsp;AZT+3TC+EFV', 'AZT+3TC+EFV', 'AZT', 12, '3TC', 10, 'EFV', 6, 0, 0, 5),
+	(3, '2 &nbsp;TDF+3TC+NVP', 'TDF+3TC+NVP', 'TDF', 16, '3TC', 10, 'NVP', 8, 0, 0, 2),
+	(4, '1 &nbsp;TDF+3TC+EFV', 'TDF+3TC+EFV', 'TDF', 16, '3TC', 10, 'EFV', 6, 0, 0, 1),
+	(5, '10 &nbsp;AZT+3TC+LPVr', 'AZT+3TC+LPVr', 'AZT', 12, '3TC', 10, '', 0, 0, 10),
+	(6, '17 &nbsp;AZT+3TC+ABC', 'AZT+3TC+ABC', 'AZT', 12, '3TC', 10, 'ABC', 11, 0, 0, 17),
+	(7, '18 &nbsp;TDF+3TC+LPVr', 'TDF+3TC+LPVr', 'TDF', 16, '3TC', 10, '', 0, 0, 18),
+	(8, '14 &nbsp;AZT+3TC+ATVr', 'AZT+3TC+ATVr', 'AZT', 12, '3TC', 10, '', 0, 0, 14),
+	(9, '11 &nbsp;TDF+3TC+ATVr', 'TDF+3TC+ATVr', 'TDF', 16, '3TC', 10, '', 0, 0, 11),
+	(10, '13 &nbsp;ABC+3TC+ATVr', 'ABC+3TC+ATVr', 'ABC', 11, '3TC', 10, '', 0, 0, 13),
+	(11, '6 &nbsp;ABC+3TC+NVP', 'ABC+3TC+NVP', 'ABC', 11, '3TC', 10, 'NVP', 8, 0, 0, 6),
+	(12, '7 &nbsp;ABC+3TC+EFV', 'ABC+3TC+EFV', 'ABC', 11, '3TC', 10, 'EFV', 6, 0, 0, 7),
+	(13, '9 &nbsp;ABC+3TC+LPVr', 'ABC+3TC+LPVr', 'ABC', 11, '3TC', 10, '', 0, 0, 9),
+	(14, '16 &nbsp;Other', 'Other', '', 0, 0, 16),
+	(15, '19 &nbsp;None', 'None', '', 0, 0, 19),
+	(16, '20 &nbsp;No Data', 'No Data', '', 0, 0, 20),
+	(17, '3 &nbsp;TDF+3TC+DTG', 'TLD', 'TDF', 16, '3TC', 10, 'DTG', 2, 0, 0, 3),
+	(18, '8 &nbsp;ABC+3TC+DTG', 'ABC+3TC+DTG', 'ABC', 11, '3TC', 10, 'DTG', 2, 0, 0, 8),
+	(19, '15 &nbsp;AZT+3TC+DRV/r', 'AZT+3TC+DRV/r', 'AZT', 12, '3TC', 10, '', 0, 0, 15);
+
+
+
+DROP TABLE IF EXISTS `regimen_classes`;
+CREATE TABLE `regimen_classes` (
+  `id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(25) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `drug_class` varchar(25) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `short_name` varchar(25) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `call` varchar(25) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+INSERT INTO `regimen_classes` VALUES 
+	(1,NULL,'INSTI','BIC','LC'),
+	(2,NULL,'INSTI','DTG','LC'),
+	(3,NULL,'INSTI','EVG','LC'),
+	(4,NULL,'INSTI','RAL','LC'),
+	(5,NULL,'NNRTI','DOR','I'),
+	(6,NULL,'NNRTI','EFV','R'),
+	(7,NULL,'NNRTI','ETR','I'),
+	(8,NULL,'NNRTI','NVP','R'),
+	(9,NULL,'NNRTI','RPV','I'),
+	(10,NULL,'NRTI','3TC','R'),
+	(11,NULL,'NRTI','ABC','R'),
+	(12,NULL,'NRTI','AZT','S'),
+	(13,NULL,'NRTI','D4T','R'),
+	(14,NULL,'NRTI','DDI','R'),
+	(15,NULL,'NRTI','FTC','R'),
+	(16,NULL,'NRTI','TDF','I'),
+	(17,NULL,'PI','ATV/r','I'),
+	(18,NULL,'PI','DRV/r','I'),
+	(19,NULL,'PI','FPV/r','R'),
+	(20,NULL,'PI','IDV/r','I'),
+	(21,NULL,'PI','LPV/r','I'),
+	(22,NULL,'PI','NFV','R'),
+	(23,NULL,'PI','SQV/r','R'),
+	(24,NULL,'PI','TPV/r','I');
