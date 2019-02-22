b29a6b9217b78dab77730d266fe0cdf3f11db587
Bakasa Joshua <baksajoshua09@gmail.com>
2019-02-22 15:03:17 +0300

    Add soft deletes

--- database/migrations/2019_02_22_053524_new_taqman_deliveries.php
+++ database/migrations/2019_02_22_053524_new_taqman_deliveries.php
@@ -16,34 +16,37 @@ class NewTaqmanDeliveries extends Migration
     public function up()
     {
         Schema::create('taqmandeliveries', function (Blueprint $table) {
-            $table->increments('id');
-            $table->tinyInteger('testtype')->unsigned()->index();
-            $table->tinyInteger('lab_id')->unsigned()->index();
-            $table->tinyInteger('quarter')->unsigned();
-            $table->integer('year')->unsigned();
-            $table->tinyInteger('source')->unsigned()->nullable();
-            $table->tinyInteger('labfrom')->unsigned()->nullable();
-            $table->string('kitlotno', 12)->nullable();
-            $table->date('expirydate')->nullable();
-            foreach ($this->main as $key => $value) {
-                foreach ($this->sub as $subkey => $subvalue) {
-                    $table->integer($value.$subvalue)->unsigned()->nullable();
-                }
-            }
-            $table->integer('receivedby')->unsigned()->index()->nullable();
-            $table->date('datereceived')->nullable();
-            $table->tinyInteger('status')->unsigned()->default(0);
-            $table->integer('enteredby')->unsigned()->index()->nullable();
-            $table->date('dateentered')->nullable();
-            $table->tinyInteger('flag')->unsigned()->default(1);
-            // $table->date('datesynchronized')->nullable();
-            $table->tinyInteger('synched')->default(0);
-            $table->date('datesynched')->nullable();
-            $table->softDeletes();
-            $table->timestamps();
-
-            $table->index(['year', 'quarter'], 'year_quarter');
+            $table->softDeletes()->after('datesynched');
         });
+        // Schema::create('taqmandeliveries', function (Blueprint $table) {
+        //     $table->increments('id');
+        //     $table->tinyInteger('testtype')->unsigned()->index();
+        //     $table->tinyInteger('lab_id')->unsigned()->index();
+        //     $table->tinyInteger('quarter')->unsigned();
+        //     $table->integer('year')->unsigned();
+        //     $table->tinyInteger('source')->unsigned()->nullable();
+        //     $table->tinyInteger('labfrom')->unsigned()->nullable();
+        //     $table->string('kitlotno', 12)->nullable();
+        //     $table->date('expirydate')->nullable();
+        //     foreach ($this->main as $key => $value) {
+        //         foreach ($this->sub as $subkey => $subvalue) {
+        //             $table->integer($value.$subvalue)->unsigned()->nullable();
+        //         }
+        //     }
+        //     $table->integer('receivedby')->unsigned()->index()->nullable();
+        //     $table->date('datereceived')->nullable();
+        //     $table->tinyInteger('status')->unsigned()->default(0);
+        //     $table->integer('enteredby')->unsigned()->index()->nullable();
+        //     $table->date('dateentered')->nullable();
+        //     $table->tinyInteger('flag')->unsigned()->default(1);
+        //     // $table->date('datesynchronized')->nullable();
+        //     $table->tinyInteger('synched')->default(0);
+        //     $table->date('datesynched')->nullable();
+        //     $table->softDeletes();
+        //     $table->timestamps();
+
+        //     $table->index(['year', 'quarter'], 'year_quarter');
+        // });
     }
 
     /**
