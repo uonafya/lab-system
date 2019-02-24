fb2e48a4657c47d4cee109e1f67fbd14bf61265f
Joel <joelkith@gmail.com>
2019-02-24 22:20:40 +0300

    Transfer Samples

--- app/Http/Controllers/SampleController.php
+++ app/Http/Controllers/SampleController.php
@@ -954,6 +954,7 @@ class SampleController extends Controller
                         return $query->where('facility_id', $facility_id);
                     })
                     ->whereNull('datetested')
+                    ->where(['repeatt' => 0])
                     ->where('created_at', '>', date('Y-m-d', strtotime("-3 months")))
                     ->paginate(25);
 
