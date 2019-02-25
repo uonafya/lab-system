8052310f8dc178d732ab3d63e4f2f739524e6a74
Bakasa Joshua <baksajoshua09@gmail.com>
2019-02-25 08:59:04 +0300

    Flattening the new array

--- app/Http/Controllers/ViralsampleController.php
+++ app/Http/Controllers/ViralsampleController.php
@@ -1076,8 +1076,9 @@ class ViralsampleController extends Controller
             $newData[] = ['Test Type','TestingLab','SpecimenLabelID','SpecimenClientCode','FacilityName','MFLCode','Sex','PMTCT','Age','DOB','SampleType','DateCollected','CurrentRegimen','regimenLine','ART Init Date','Justification','DateReceived','loginDate','ReceivedStatus','RejectedReason','ReasonforRepeat','LabComment','Datetested','DateDispatched','Results','Edited'];
             // dd($data);
             foreach ($data as $key => $sample) {
-                // $sample = (array)$sample;
+                $sample = collect($sample)->flatten(1)->toArray();
                 // dd($sample[3]);
+                // $sample = (array)$sample;
                 $dbsample = ViralsampleView::where('patient', '=', $sample[3])->where('datecollected', '=', $sample[11])->first();
                 $sample[19] = $dbsample->rejectedreason ?? null;
                 $sample[20] = $dbsample->reason_for_repeat ?? null;
