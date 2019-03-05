c7a71275de4073dd4f29efe1153182b75520213b
Bakasa Joshua <baksajoshua09@gmail.com>
2019-03-05 13:06:21 +0300

    Use PHP empty function

--- app/Http/Controllers/ViralbatchController.php
+++ app/Http/Controllers/ViralbatchController.php
@@ -657,7 +657,7 @@ class ViralbatchController extends Controller
                 $batch->received_by = auth()->user()->id;
                 $batch->datereceived = date('Y-m-d');
                 $batch->pre_update();
-                if(!$batch->samples->isEmpty()){
+                if(!empty($batch->samples)){
                     foreach ($batch->samples as $sample) {
                         $sample->sample_received_by = auth()->user()->id;
                         $sample->pre_update();
