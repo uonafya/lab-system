b4e87a84ed8126c0f8c10d5f5b3e760988bc78e4
Joel <joelkith@gmail.com>
2019-03-05 19:30:52 +0300

    Updates

--- resources/views/tables/viralworksheets.blade.php
+++ resources/views/tables/viralworksheets.blade.php
@@ -39,10 +39,10 @@
     <div class="row">
         <div class="col-md-12">
             Click To View: 
-            <a href="{{ $myurl2 . 1 }}" title="Taqman Worksheets"> Taqman Worksheets </a> |
-            <a href="{{ $myurl2 . 2 }}" title="Abbott Worksheets"> Abbott Worksheets </a> |
-            <a href="{{ $myurl2 . 3 }}" title="C8800 Worksheets"> C8800 Worksheets </a> |
-            <a href="{{ $myurl2 . 4 }}" title="Pantha Worksheets"> Pantha Worksheets </a> |
+            <a href="{{ $myurl2 . '/1'  }}" title="Taqman Worksheets"> Taqman Worksheets </a> |
+            <a href="{{ $myurl2 . '/2' }}" title="Abbott Worksheets"> Abbott Worksheets </a> |
+            <a href="{{ $myurl2 . '/3' }}" title="C8800 Worksheets"> C8800 Worksheets </a> |
+            <a href="{{ $myurl2 . '/4' }}" title="Pantha Worksheets"> Pantha Worksheets </a> |
         </div>        
     </div>
     
