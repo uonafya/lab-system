e07092dbfda875d785729d92ba12ddd2c60420a6
Joel <joelkith@gmail.com>
2019-03-19 18:01:32 +0300

    Updates

--- app/Http/Controllers/ViralbatchController.php
+++ app/Http/Controllers/ViralbatchController.php
@@ -480,15 +480,15 @@ class ViralbatchController extends Controller
             ->where('lab_id', env('APP_LAB'))
             ->get();
 
-        $noresult_a = MiscViral::get_totals(0);
-        $redraw_a = MiscViral::get_totals(5);
-        $failed_a = MiscViral::get_totals(3);
-        $detected_a = MiscViral::get_totals(2);
-        $undetected_a = MiscViral::get_totals(1);
+        $noresult_a = MiscViral::get_totals(0, null, true);
+        $redraw_a = MiscViral::get_totals(5, null, true);
+        $failed_a = MiscViral::get_totals(3, null, true);
+        $detected_a = MiscViral::get_totals(2, null, true);
+        $undetected_a = MiscViral::get_totals(1, null, true);
 
-        $rejected = MiscViral::get_rejected();
-        $date_modified = MiscViral::get_maxdatemodified();
-        $date_tested = MiscViral::get_maxdatetested();
+        $rejected = MiscViral::get_rejected(null, true);
+        $date_modified = MiscViral::get_maxdatemodified(null, true);
+        $date_tested = MiscViral::get_maxdatetested(null, true);
         $currentdate=date('d-m-Y');
 
         $batches->transform(function($batch, $key) use ($noresult_a, $redraw_a, $failed_a, $detected_a, $undetected_a, $rejected, $date_modified, $date_tested){
