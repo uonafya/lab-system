68c0fd840a088b10cb9b3426f560d6ce30386461
Joel <joelkith@gmail.com>
2019-03-22 08:00:14 +0300

    Updates

--- app/Misc.php
+++ app/Misc.php
@@ -514,7 +514,7 @@ class Misc extends Common
         $create = true;
         if($count == $machine->eid_limit) $create = true;
         if($temp_limit && $count == $temp_limit) $create = true;
-        if(in_array(env('APP_LAB'), [9])) $create = true;
+        if(in_array(env('APP_LAB'), [9, 5])) $create = true;
 
         return [
         	'count' => $count, 'limit' => $temp_limit,
