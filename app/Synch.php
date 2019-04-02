ba90236443f7d0f065b5b216e710041f0aef9794
Joel <joelkith@gmail.com>
2019-04-02 22:15:32 +0300

    Updates

--- app/Synch.php
+++ app/Synch.php
@@ -1367,54 +1367,57 @@ class Synch
 		}
 	}
 
+    public static function synch_facilities()
+    {
+        ini_set('memory_limit', '-1');
+        $client = new Client(['base_uri' => self::$base]);
+        $today = date('Y-m-d');
 
+        $max_temp = FacilityChange::selectRaw("max(temp_facility_id) as maximum ")->first()->maximum ?? 1000000;
 
-	public static function synch_facilities()
-	{
-		ini_set('memory_limit', '-1');
-		$client = new Client(['base_uri' => self::$base]);
-		$today = date('Y-m-d');
+        while (true) {
+            $facilities = Facility::where('synched', 0)->limit(30)->get();
+            if($facilities->isEmpty()) break;
 
-		while (true) {
-			$facilities = Facility::where('synched', 0)->limit(30)->get();
-			if($facilities->isEmpty()) break;
+            $response = $client->request('post', 'facility', [
+                'headers' => [
+                        'Accept' => 'application/json',
+                        'Authorization' => 'Bearer ' . self::get_token(),
+                ],
+                'json' => [
+                        'facilities' => $facilities->toJson(),
+                        'lab_id' => env('APP_LAB', null),
+                ],
+            ]);
 
-			$response = $client->request('post', 'facility', [
-				'headers' => [
-					'Accept' => 'application/json',
-					'Authorization' => 'Bearer ' . self::get_token(),
-				],
-				'json' => [
-					'facilities' => $facilities->toJson(),
-					'lab_id' => env('APP_LAB', null),
-				],
+            $body = json_decode($response->getBody());
 
-			]);
+            $update_data = ['synched' => 1,];
 
-			$body = json_decode($response->getBody());
+            foreach ($body->facilities as $key => $value) {
+                Facility::where('id', $value->old_facility_id)->update($update_data);
 
-			dd($body);
+                if($value->new_facility_id != $value->old_facility_id){
 
-			foreach ($body->facilities as $key => $value) {
-				$update_data = ['id' => $value->id, 'synched' => 1,];
-				Facility::where('id', $value->original_id)->update($update_data);
-				if($value->id != $value->original_id){
-					self::change_facility_id(Batch::class, $value->original_id, $value->id);
-					self::change_facility_id(Viralbatch::class, $value->original_id, $value->id);
-					self::change_facility_id(Patient::class, $value->original_id, $value->id);
-					self::change_facility_id(Viralpatient::class, $value->original_id, $value->id);
-				}
-			}
-		}
-	}
+                    $f = new FacilityChange;
+                    $f->old_facility_id = $value->old_facility_id;
+                    $f->new_facility_id = $value->new_facility_id;
+                    $f->temp_facility_id = $max_temp++;
+                    $f->save();
 
-	public static function change_facility_id($class_name, $old_id, $new_id)
-	{
-		$models = $class_name::where('facility_id', $old_id)->get();
-		foreach ($models as $m) {
-			$m->facility_id = $new_id;
-			$m->pre_update();
-		}
-	}
+                    \App\Common::change_facility_id($value->old_facility_id, $f->temp_facility_id, true);
+                }
+            }
+        }
+
+        $changes = FacilityChange::where(['implemented' => 0])->get();
+
+        foreach ($changes as $f) {
+            \App\Common::change_facility_id($f->temp_facility_id, $f->new_facility_id, true);
+            $f->implemented = 1;
+            $f->save();
+        }
+    }
+
 
 }
