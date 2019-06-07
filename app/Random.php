5496fb69159fe65831667badf91ba54d071007f6
Joshua Bakasa <baksajoshua09@gmail.com>
2019-06-07 14:23:32 +0300

    Missing EDARP results

--- app/Random.php
+++ app/Random.php
@@ -2039,7 +2039,7 @@ class Random
 
 	public static function export_edarp_results() {
         echo "==> Retrival Begin \n";
-		$file = 'public/docs/EDARP_samples_on_KEMRI_752019.xlsx';
+		$file = 'public/docs/MISSING RESULTS.xlsx';
         // $batch = null;
         // $lookups = Lookup::get_viral_lookups();
         // dd($lookups);
