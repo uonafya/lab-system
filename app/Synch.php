fda495eb7ff17a01555d4346e364d63600b6282f
Joel <joelkith@gmail.com>
2019-07-04 10:12:55 +0300

    Updates

--- app/Synch.php
+++ app/Synch.php
@@ -153,6 +153,29 @@ class Synch
 		return false;
 	}
 
+	public static function clean_emails()
+	{
+		$base = 'https://api.mailgun.net/v3/nascop.or.ke/complaints';
+		$client = new Client(['base_uri' => $base]);
+		$response = $client->request('get', '', [
+			'auth' => ['api', env('MAIL_API_KEY')],
+		]);
+		$body = json_decode($response->getBody());
+		if($response->getStatusCode() > 399) return false;
+
+		$emails = [];
+
+		foreach ($body->items as $key => $value) {
+			$emails[] = $value->address;
+		}
+
+		\App\Facility::whereIn('email', $emails)->update(['emails' => null]);
+		\App\Facility::whereIn('contact_email', $emails)->update(['contact_email' => null]);
+
+		\App\FacilityContact::whereIn('email', $emails)->update(['emails' => null]);
+		\App\FacilityContact::whereIn('contact_email', $emails)->update(['contact_email' => null]);
+	}
+
 	public static function login()
 	{
 		Cache::store('file')->forget('api_token');
