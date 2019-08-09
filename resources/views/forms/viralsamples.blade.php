231f63a2551324fd7df6925e3cb7d84f24d1426e
Joel <joelkith@gmail.com>
2019-08-09 12:30:28 +0300

    New Lab Form Changes

--- resources/views/forms/viralsamples.blade.php
+++ resources/views/forms/viralsamples.blade.php
@@ -472,7 +472,7 @@
                                             selected
                                         @endif
 
-                                        > {{ $proph->code . ' ' . $proph->name }}
+                                        > {{ $proph->code . ' - ' . $proph->name }}
                                         </option>
 
 
