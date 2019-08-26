8d9873fab79c50cfde95371fd3e0201e9c61b8cb
Joel <joelkith@gmail.com>
2019-08-26 15:02:34 +0300

    Drug Resistance Updates

--- app/Synch.php
+++ app/Synch.php
@@ -300,7 +300,7 @@ class Synch
 		while (true) {
 			$batches = $batch_class::with(['sample.patient:id,national_patient_id,patient'])
 			->where('synched', 0)->where('id', 159819)->where('batch_complete', 1)->limit(20)->get();
-			// dd($batches);
+			dd($batches);
 			if($batches->isEmpty()) break;
 
 			$response = $client->request('post', $url, [
