3383a2a96d8bd9b9121c9b678f352a044407e49e
Joel <joelkith@gmail.com>
2019-09-25 16:28:03 +0300

    Updates

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -36,7 +36,7 @@ class Kernel extends HttpKernel
 
             \App\Http\Middleware\TrimStrings::class,
             \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-            \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
+            // \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         ],
 
         'api' => [
