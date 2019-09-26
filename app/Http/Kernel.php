e35ed4f2ce25f271544b05d7e81f3dc0c458f24f
Joshua Bakasa <baksajoshua09@gmail.com>
2019-09-26 08:57:11 +0300

    Added Trim String Middleware manually

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -17,6 +17,7 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
+        \Illuminate\Foundation\Http\Middleware\TrimStrings::class,
     ];
 
     /**
