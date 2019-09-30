5b59374600930cd9e5e737ab94bfb60896e0fae8
Joel <joelkith@gmail.com>
2019-09-30 22:28:51 +0300

    Suscepibility Report

--- app/Exports/DrSusceptabilityExport.php
+++ app/Exports/DrSusceptabilityExport.php
@@ -118,6 +118,7 @@ class DrSusceptabilityExport extends BaseExport implements FromArray, WithEvents
 
             	$event->sheet->styleCells($my_cell, [
             		'fill' => [
+            			'fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID,
             			'startColor' => [
             				// 'argb' => $my_call['resistance_colour']
             				'argb' => 'FFA0A0A0'
