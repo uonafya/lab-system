14b7000b16a57844331e81660cd350080b271c13
Joel <joelkith@gmail.com>
2019-10-10 12:57:52 +0300

    Updates

--- resources/views/worksheets/labels.blade.php
+++ resources/views/worksheets/labels.blade.php
@@ -1,41 +1,13 @@
 <html>
 <link rel="stylesheet" type="text/css" href="{{ asset('css/worksheet_style.css') }}" media="screen" />
+
+
 <style type="text/css">
-<!--
-.style1 {font-family: "Courier New", Courier, monospace}
-.style4 {font-size: 12}
-.style5 {font-family: "Courier New", Courier, monospace; font-size: 12; }
-.style7 {font-size: x-small}
--->
-</style>
-<style>
-
- td
- {
-
- }
- .oddrow
- {
- background-color : #CCCCCC;
- }
- .evenrow
- {
- background-color : #F0F0F0;
- } #table1 {
-border : solid 1px black;
-width:1100px;
-width:1180px;
-}
- /*.style7 {font-size: medium}*/
-.style10 {font-size: 16px}
-</style>
-
-<STYLE TYPE="text/css">
      P.breakhere {page-break-before: always}
 
 }
 
-</STYLE> 
+</style> 
 <body onLoad="JavaScript:window.print();">
 	<div align="center">
 		<table border="0" class="data-table" align='center'>
