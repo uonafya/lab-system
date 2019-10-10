3cabdc5bc5a532d55b24c813b1f9eff11a9a2509
Joel <joelkith@gmail.com>
2019-10-10 09:38:41 +0300

    Updates

--- app/Http/Controllers/ViralbatchController.php
+++ app/Http/Controllers/ViralbatchController.php
@@ -283,7 +283,7 @@ class ViralbatchController extends Controller
 
     public function labels(Viralbatch $batch)
     {
-        $samples = ViralsampleView::select('id')->where(['batch_id' => $batch->id])->where('repeatt', '!=', 1)->where('receivedstatus', '!=', 2)->get();
+        $samples = ViralsampleView::select('id')->where(['batch_id' => $batch->id])->where('repeatt', '!=', 1)->whereRaw('(receivedstatus IS NULL or receivedstatus=1)')->get();
         return view('worksheets.labels', ['samples' => $samples]);
     }
 
