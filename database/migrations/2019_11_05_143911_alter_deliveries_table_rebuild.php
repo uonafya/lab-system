158fefa44b1ec91508e2fffa6f189e5458381a53
Joshua Bakasa <baksajoshua09@gmail.com>
2019-11-05 15:24:04 +0300

    Kit deliveries updates

---
+++ database/migrations/2019_11_05_143911_alter_deliveries_table_rebuild.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterDeliveriesTableRebuild extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::dropIfExists('deliveries');
+        Schema::create('deliveries', function(Blueprint $table){
+            $table->bigIncrements();
+            $table->bigInteger('national_id')->nullable();
+            $table->tinyInteger('quarter')->comment('This is the quarter in which the delivery was done');
+            $table->integer('year')->comment('This is the quarter in which the delivery was done');
+            $table->tinyInteger('type')->nullable()->comment("Indicates if this is an EID, VL, Consumable delivery");
+            $table->tinyInteger('lab_id');
+            $table->bigInteger('receivedby');
+            $table->dete('datereceived');
+            $table->bigInteger('enteredby');
+            $table->date('dateentered');
+            $table->tinyInteger('synched')->default(0);
+            $table->dateTime('datesynched')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('deliveries');
+    }
+}
---
+++ database/migrations/2019_11_05_144119_create_delivery_details_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDeliveryDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('delivery_details', function (Blueprint $table) {
+            $table->increments('id');
+            $table->bigInteger('national_id')->nullable();
+            $table->tinyInteger('synched')->default(0);
+            $table->dateTime('datesynched')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('delivery_details');
+    }
+}
