a02c822fb9d2ac16064d18e22fb27c9f73f2ef1d
Joshua Bakasa <baksajoshua09@gmail.com>
2019-11-11 14:41:56 +0300

    Update the views

--- app/ViralsampleCompleteView.php
+++ app/ViralsampleCompleteView.php
@@ -7,15 +7,20 @@ use DB;
 
 class ViralsampleCompleteView extends ViewModel
 {	
-	protected $table = 'viralsample_complete_view';
+   protected $table = 'viralsample_complete_view';
 
-	public static function run() {
-		DB::statement("CREATE OR REPLACE VIEW viralsample_complete_view AS
-        (
-          SELECT s.*, b.highpriority, b.datereceived, b.datedispatched, b.site_entry, b.lab_id, b.facility_id, b.batch_complete,
-          p.patient_status, p.patient, p.sex, p.dob, p.initiation_date, g.gender_description, rs.name as receivedstatus_name, vp.name as prophylaxis_name, vj.name as justification_name, vs.name as sampletype_name, vpt.name as pmtct_name, vr.name as rejected_name
+   public function worksheet()
+   {
+      return $this->belongsTo('App\Viralworksheet', 'worksheet_id');
+   }
 
-          FROM viralsamples s
+   public static function run() {
+      DB::statement("CREATE OR REPLACE VIEW viralsample_complete_view AS
+         (
+            SELECT s.*, b.highpriority, b.datereceived, b.datedispatched, b.site_entry, b.lab_id, b.facility_id, b.batch_complete,
+            p.patient_status, p.patient, p.sex, p.dob, p.initiation_date, g.gender_description, rs.name as receivedstatus_name, vp.name as prophylaxis_name, vj.name as justification_name, vs.name as sampletype_name, vpt.name as pmtct_name, vr.name as rejected_name
+
+            FROM viralsamples s
             JOIN viralbatches b ON b.id=s.batch_id
             JOIN viralpatients p ON p.id=s.patient_id
             LEFT JOIN gender g on g.id=p.sex
@@ -26,6 +31,6 @@ class ViralsampleCompleteView extends ViewModel
             LEFT JOIN viralpmtcttype vpt on vpt.id=s.pmtct
             LEFT JOIN viralrejectedreasons vr on vr.id=s.rejectedreason
 
-        );");
-	}
+         );");
+   }
 }
