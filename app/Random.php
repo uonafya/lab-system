bbf5d2351d34f935c25c1debadb16f7298b83031
Joel <joelkith@gmail.com>
2019-12-03 11:49:54 +0300

    Create Profile

--- app/Random.php
+++ app/Random.php
@@ -243,7 +243,7 @@ class Random
         $sql = "YEAR(datetested) AS `Year`, MONTH(datetested) AS `Month`, count(id) AS `Number of Valid Baseline Tests` ";
 
         $rows = \App\ViralsampleView::selectRaw($sql)
-            ->where(['repeatt' => 0, 'receivedstatus' => 1, 'lab_id' => env('APP_LAB')])
+            ->where(['justification' => 10, 'repeatt' => 0, 'receivedstatus' => 1, 'lab_id' => env('APP_LAB')])
             ->whereBetween('rcategory', [1,4])
             ->where('datetested', '>', '2014-12-31')
             ->groupBy('Year', 'Month')
