8148bc23c03563fd027384d48d542ea9ba7e8d92
Joel <joelkith@gmail.com>
2020-01-23 17:46:21 +0300

    Updates

--- app/Nat.php
+++ app/Nat.php
@@ -29,7 +29,7 @@ class Nat
 		Mail::to($emails)->send(new TestMail($attachments));
 	}
 
-	public static function get_county_current_query($year, $paed=false, $suppressed=true)
+	public static function get_county_current_query($year, $paed=false, $suppressed=true, $ages=true, $suppression=true)
 	{
     	$sql = 'SELECT f.county_id, sex, count(*) as totals ';
 		$sql .= 'FROM ';
@@ -40,18 +40,22 @@ class Nat
 		$sql .= 'FROM viralsamples_view ';
 		$sql .= "WHERE ( datetested between '{$year}-01-01' and '{$year}-12-31' ) ";
 		$sql .= "AND patient != '' AND patient != 'null' AND patient is not null ";
-		if($paed) $sql .= "AND age < 15 ";
-		else{
-			$sql .= "AND age > 14 ";
+		if($ages){
+			if($paed) $sql .= "AND age < 15 ";
+			else{
+				$sql .= "AND age > 14 ";
+			}
 		}
 		$sql .= 'AND flag=1 AND repeatt=0 AND rcategory in (1, 2, 3, 4) ';
 		$sql .= 'AND justification != 10 and facility_id != 7148 ';
 		$sql .= 'GROUP BY patient_id) gv ';
 		$sql .= 'ON v.id=gv.id) tb ';
 		$sql .= 'JOIN view_facilitys f on f.id=tb.facility_id ';
-		if($suppressed) $sql .= 'WHERE rcategory IN (1,2) ';
-		else{
-			$sql .= 'WHERE rcategory IN (3,4) ';
+		if($suppression){
+			if($suppressed) $sql .= 'WHERE rcategory IN (1,2) ';
+			else{
+				$sql .= 'WHERE rcategory IN (3,4) ';
+			}
 		}
 		$sql .= 'GROUP BY f.county_id, sex ';
 		$sql .= 'ORDER BY f.county_id, sex ';
@@ -66,11 +70,12 @@ class Nat
 
 		$counties = DB::table('countys')->get();
 
-		for ($year=2013; $year < 2020; $year++) { 
+		for ($year=2019; $year < 2020; $year++) { 
 			$paeds_sup = self::get_county_current_query($year, true);
 			$paeds = self::get_county_current_query($year, true, false);
 			$adults_sup = self::get_county_current_query($year, false);
 			$adults = self::get_county_current_query($year, false, false);
+			$all = self::get_county_current_query($year, false, false, false, false);
 			foreach ($counties as $county) {
 				$row = ['Year' => $year, 'County' => $county->name];
 
@@ -85,6 +90,9 @@ class Nat
 
 				$row['Adult Non-Suppressed Male'] = $adults->where('county_id', $county->id)->where('sex', 1)->first()->totals ?? 0;
 				$row['Adult Non-Suppressed Female'] = $adults->where('county_id', $county->id)->where('sex', 2)->first()->totals ?? 0;
+
+				$row['All Male'] = $adults->where('county_id', $county->id)->where('sex', 1)->first()->totals ?? 0;
+				$row['All Female'] = $adults->where('county_id', $county->id)->where('sex', 2)->first()->totals ?? 0;
 
 				$data[] = $row;
 			}
