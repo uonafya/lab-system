ab8dc942e5aa28e431879d5b08ac4e001b02b97b
Joel <joelkith@gmail.com>
2020-04-13 10:02:20 +0300

    Covid Bug Fixes

--- app/Common.php
+++ app/Common.php
@@ -57,6 +57,26 @@ class Common
 		],
 	];
 
+	
+
+	public static function csv_download($data)
+	{
+		$fp = fopen('php://output', 'w');
+
+		$first = [];
+
+		foreach ($data[0] as $key => $value) {
+			$first[] = $key;
+		}
+		fputcsv($fp, $first);
+
+		foreach ($data as $key => $value) {
+			fputcsv($fp, $value);
+		}
+		
+		fclose($fp);
+	}
+
     public static function test_email()
     {
         Mail::to(['joelkith@gmail.com', 'baksajoshua09@gmail.com'])->send(new TestMail());
