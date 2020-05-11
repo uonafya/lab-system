6936d46ce6625684ad4533459a711586240b39ca
Joshua Bakasa <baksajoshua09@gmail.com>
2020-05-12 01:28:30 +0300

    Manual testing labs kits consumption submission and reports

--- app/CovidConsumption.php
+++ app/CovidConsumption.php
@@ -18,8 +18,15 @@ class CovidConsumption extends BaseModel
 
     public function getTestsDone($start_date, $end_date)
     {
+        $user = auth()->user();
         return CovidSample::whereBetween('datetested', [$start_date, $end_date])
-                    ->where('receivedstatus', '<>', 2)->get()->count();
+                    ->where('receivedstatus', '<>', 2)
+                    ->when($user, function($query) use ($user){
+                        if ($user->user_type_id == 12)
+                            return $query->where('lab_id', '=', $user->lab_id);
+                        else
+                            return $query->where('lab_id', '=', env('APP_LAB'));
+                    })->get()->count();
     }
 
     public function synchComplete()
@@ -42,24 +49,59 @@ class CovidConsumption extends BaseModel
 
     public function getMissingConsumptions()
     {
-        $previous_weeks = [
-                        'first' => '2020-04-06',
-                        'second' => '2020-04-13',
-                        'third' => '2020-04-20',
-                    ];
         $data = [];
-        foreach ($previous_weeks as $key => $week) {
-            if ($key == 'first' && env('APP_LAB') == 1){
-                if ($this->getdata($week))
-                    $data[] = $this->getdata($week);
-            } else if (in_array(env('APP_LAB'), [1, 5, 9]) && $key != 'first') {
-                if ($this->getdata($week))
-                    $data[] = $this->getdata($week);
+        $user = auth()->user();
+
+        // Getting the first date covid tests where done
+        $firsttestdate = CovidSampleView::selectRaw("min(`datetested`) AS `datetested`")
+            ->whereNotNull('datetested')
+            ->when($user, function ($query) use ($user) {
+                if ($user->user_type_id == 12)
+                    return $query->where('lab_id', '=', $user->lab_id);
+                else
+                    return $query->where('lab_id', '=', env('APP_LAB'));
+            })->first();
+
+        if (null !== $firsttestdate->datetested) { // Tests exists, get the weeks that need to be reported on
+            $date = $firsttestdate->datetested;
+            $lastweeek = $this->getPreviousWeek();
+            $loop = true;
+            while ($loop) {
+                $week = $this->getStartAndEndDate(date('W', strtotime($date)), date('Y', strtotime($date)));
+                if ($this->getdata($week->week_start))
+                    $data[] = $week;
+                $date = date('Y-m-d', strtotime("+1 day",strtotime($week->week_end)));
+                if ($week->week == $lastweeek->week)
+                    $loop = false;
             }
-            
+        } else {// Tests not done report for last week
+            $week = $this->getPreviousWeek();
+            if ($this->getdata($week->week_start))
+                $data[] = $this->getPreviousWeek();
         }
-        $data[] = $this->getPreviousWeek();
+
         return $data;
+
+        // $lastweeek = $this->getPreviousWeek();
+        // dd($firsttestdate);
+        // $previous_weeks = [
+        //                 'first' => '2020-04-06',
+        //                 'second' => '2020-04-13',
+        //                 'third' => '2020-04-20',
+        //             ];
+        // $data = [];
+        // foreach ($previous_weeks as $key => $week) {
+        //     if ($key == 'first' && env('APP_LAB') == 1){
+        //         if ($this->getdata($week))
+        //             $data[] = $this->getdata($week);
+        //     } else if (in_array(env('APP_LAB'), [1, 5, 9]) && $key != 'first') {
+        //         if ($this->getdata($week))
+        //             $data[] = $this->getdata($week);
+        //     }
+            
+        // }
+        // $data[] = $this->getPreviousWeek();
+        // return $data;
     }
 
     public function fillTestsDone()
