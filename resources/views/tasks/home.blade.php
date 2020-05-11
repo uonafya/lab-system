f7ad294239c8f947ffbdead13d6394f78e584999
Joshua Bakasa <baksajoshua09@gmail.com>
2020-05-11 09:24:04 +0300

    Debug Kisumu

--- resources/views/tasks/home.blade.php
+++ resources/views/tasks/home.blade.php
@@ -47,7 +47,7 @@
                         </div>
                     @else 
                         <div class="alert alert-success spacing bottom">
-                            <strong><a href="#" style="color: black;">Last week`s ({{ $data->time->first()->week_start }} - {{ $data->time->first()->week_end }}) COVID consumptions Submitted</a></strong>
+                            <strong><a href="#" style="color: black;">Last week`s ({{ $data->time[0]->week_start }} - {{ $data->time[0]->week_end }}) COVID consumptions Submitted</a></strong>
                         </div>
                     @endif
                     <!-- Kit and kits consumption -->
