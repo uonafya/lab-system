f3187d12e4a0beb0220b1366b6e8d009d6855b4c
Joel <joelkith@gmail.com>
2020-05-16 15:36:38 +0300

    Transfer Covid Samples

--- resources/views/layouts/sidenav.blade.php
+++ resources/views/layouts/sidenav.blade.php
@@ -74,6 +74,14 @@
                 @endif
                 <li><a href="{{ url('worksheet/create/2') }}">Create Abbott Worksheet(96)</a></li>
                 <hr />
+                <li><a href="{{ url('worksheet/create/3/22') }}">Create C8800 Worksheet(24)</a></li>
+                <hr />
+                <li><a href="{{ url('worksheet/create/3/46') }}">Create C8800 Worksheet(48)</a></li>
+                <hr />
+                <li><a href="{{ url('worksheet/create/3/70') }}">Create C8800 Worksheet(72)</a></li>
+                <hr />
+                <li><a href="{{ url('worksheet/create/3') }}">Create C8800 Worksheet(96)</a></li>
+                <hr />
                 <!-- <li>
                     <a href="#"><span class="nav-label">Batches</span><span class="fa arrow"></span> </a>
                     <ul class="nav nav-second-level">
