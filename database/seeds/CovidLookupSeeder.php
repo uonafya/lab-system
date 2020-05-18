83277c861ec88159a48f8d00a72e590986877d22
Joel <joelkith@gmail.com>
2020-05-18 18:46:40 +0300

    Updates

--- database/seeds/CovidLookupSeeder.php
+++ database/seeds/CovidLookupSeeder.php
@@ -119,7 +119,7 @@ class CovidLookupSeeder extends Seeder
 			['id' => 94, 'name' => 'Naivasha hospital', 'email' => 'naivashahospital@gmail.com,lizkiptoo@gmail.com'],
 			['id' => 95, 'name' => 'Kiambu Level 5', 'email' => 'scmohkiambu@gmail.com'],
 			['id' => 96, 'name' => 'Kwibancha', 'email' => ''],
-			// ['id' => , 'name' => '', 'email' => ''],
+			['id' => 97, 'name' => 'Red Cross', 'email' => ''],
 			// ['id' => , 'name' => '', 'email' => ''],
 			// ['id' => , 'name' => '', 'email' => ''],
 		]);
