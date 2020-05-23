9ca5f833f8e4cf26ca0c479555ab10a442b8f146
Joel <joelkith@gmail.com>
2020-05-23 14:49:25 +0300

    Updates

--- resources/views/forms/covidsamples.blade.php
+++ resources/views/forms/covidsamples.blade.php
@@ -111,7 +111,7 @@
 
                         @include('partial.select', ['model' => $m, 'required' => true, 'default_val' => $sample->patient->county_id ?? null, 'prop' => 'county_id', 'label' => 'County', 'items' => $countys])
 
-                        @include('partial.select', ['model' => $m, 'required' => true, 'default_val' => $sample->patient->subcounty_id ?? null, 'prop' => 'subcounty_id', 'label' => 'Subcounty', 'items' => $districts])
+                        @include('partial.select', ['model' => $m, 'default_val' => $sample->patient->subcounty_id ?? null, 'prop' => 'subcounty_id', 'label' => 'Subcounty', 'items' => $districts])
 
                         @if(auth()->user()->quarantine_site)
                             <input type="hidden" name="quarantine_site_id" value="{{ auth()->user()->facility_id }}">
