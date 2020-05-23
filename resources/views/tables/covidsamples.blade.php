d877d4fca4c9b2e70a5e8c56f8e62298ae08e98c
Joel <joelkith@gmail.com>
2020-05-23 15:47:28 +0300

    Updates

--- resources/views/tables/covidsamples.blade.php
+++ resources/views/tables/covidsamples.blade.php
@@ -168,6 +168,8 @@
                         </div>                        
                     </div> 
                 </div>
+            </div>
+            <div class="row">
 
                 <div class="col-md-4"> 
                     <div class="form-group">
