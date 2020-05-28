6054b777bc4453c86ff9adc56817657e98773bab
Joel <joelkith@gmail.com>
2020-05-28 09:08:51 +0300

    Updates

--- resources/views/charts/bar_graph.blade.php
+++ resources/views/charts/bar_graph.blade.php
@@ -44,7 +44,7 @@
                 yDecimals: 0,
                 valueDecimale: 0,
                 headerFormat: '<table class="tip"><caption>{point.key}</caption>'+'<tbody>',
-                pointFormat: '<tr><th style="color:{series.color}">{series.name}:</th>'+'<td style="text-align:right">{point.y}' + 
+                pointFormat: '<tr><th style="color:{series.color}">{series.name}:</th>'+'<td style="text-align:right">{point.y} &nbsp;' + 
                 @if(isset($extra_tooltip))
                     '{point.z}'
                 @else
