284ebb0556c581180adf3b155fc65b63f7308738
Joel <joelkith@gmail.com>
2020-06-04 10:54:29 +0300

    a

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,9 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         // dd('What the hell do you think you are doing!! We see you!!!');
         // $this->call(UsersTableSeeder::class);
-        // dd('What the hell do you think you are doing!! We see you!!!');
-        // $this->call(UsersTableSeeder::class);
-        // $this->call(UsersTableSeeder::class);
         // $this->call(FakerSeeder::class);        
         // $this->call(DrSeeder::class);
         $this->call(KitsSeeder::class);    
