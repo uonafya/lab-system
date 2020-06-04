7f6a5ff0555c883396c387baef9907b1f11d3409
Joel <joelkith@gmail.com>
2020-06-04 11:27:34 +0300

    Updates

---
+++ app/CovidModels/CovidTravel.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CovidTravel extends BaseModel
+{
+	protected $dates = ['travel_date'];
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,13 +12,13 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // dd('What the hell do you think you are doing!! We see you!!!');
-        // $this->call(UsersTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
         // $this->call(FakerSeeder::class);        
         // $this->call(DrSeeder::class);
         $this->call(KitsSeeder::class);    
         // $this->call(LabEquipmentMailingSeeder::class);
         // $this->call(GeneralConsumablesSeeder::class);
-        // $this->call(CovidLookupSeeder::class);
+        $this->call(CovidLookupSeeder::class);
         $this->call(CovidKitsSeeder::class);
         $this->call(TestTypeSeeder::class);
         // $this->call(TestTypeSeeder::class);
