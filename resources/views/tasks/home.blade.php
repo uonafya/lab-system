f28de2d0c3efeed37841057a5c180f16d83d77c8
Joshua Bakasa <baksajoshua09@gmail.com>
2020-06-09 12:33:57 +0300

    Remove the curly brace

--- resources/views/tasks/home.blade.php
+++ resources/views/tasks/home.blade.php
@@ -74,7 +74,7 @@
                     <!-- Kit and kits consumption -->
 
                     <!-- Lab performance Report -->
-                    {@if ($performance == 0)
+                    @if ($performance == 0)
                         <div class="alert alert-warning spacing bottom">
                         	<strong><a href="{{ url('performancelog') }}">Click to Submit Monthly Lab Performance Log ( Tracker )</a></strong>
                         	<p style="margin-left: 3em;"><font color="#CCCCCC">Log on any Back logs and Reasons for the previous month</font></p>
