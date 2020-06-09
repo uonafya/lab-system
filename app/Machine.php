5be5ee2956ce65f27a691897161ffc5bf45e7451
Joshua Bakasa <baksajoshua09@gmail.com>
2020-06-09 22:44:53 +0300

    Traversing level 4

--- app/Machine.php
+++ app/Machine.php
@@ -84,7 +84,7 @@ class Machine extends Model
         $returnValue = 0;
         $data = [];
         $data = $this->getTestsFromStorage($year, $month);
-        dd($data[$type][$this->id][$year]);
+        dd($data[$type][$this->id][$year][$month]);
         $returnValue = $data[$type][$this->id][$year][$month];
         return $returnValue;
     }
