3c4f56c9e92e7e62a6f6aa6e4924774c88b9a96a
Joshua Bakasa <baksajoshua09@gmail.com>
2020-06-10 18:17:22 +0300

    getting the relevant balances

--- app/Kits.php
+++ app/Kits.php
@@ -94,10 +94,10 @@ class Kits extends BaseModel
         $lastmonth = date('Y', strtotime("-1 Month", strtotime($year . '-' . $month)));
         $balance = 0;
         if (!$this->consumption_lines->isEmpty()){
-            dd($this->consumption_lines);
             $balance = $this->consumption_headers()
                             ->where('year', $lastmonthyear)->where('month', $lastmonth)
                             ->where('type', $type)->where('machine', $this->machine_id);
+            dd($balance);
             if (!$balance->isEmpty()) {
 
             } else {
