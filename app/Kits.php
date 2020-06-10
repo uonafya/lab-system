73a0a0a2cbbd3271fafc20252058da9ef33aef14
Joshua Bakasa <baksajoshua09@gmail.com>
2020-06-10 18:38:02 +0300

    Beginning balance fix

--- app/Kits.php
+++ app/Kits.php
@@ -98,7 +98,7 @@ class Kits extends BaseModel
         //                 }])->get();
         
         if (!$this->consumption_lines->isEmpty()){
-            $balance = $this->consumption_headers();
+            $balance = $this->consumption_headers()
                             ->where('year', $lastmonthyear)->where('month', $lastmonth)
                             ->where('type', $type)
                             ->where('machine', $this->machine_id);
