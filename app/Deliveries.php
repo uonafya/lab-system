ef846f17e9db4f0fbdc78be6d24f5cb8a0a62e6d
Joshua Bakasa <baksajoshua09@gmail.com>
2020-06-15 13:36:42 +0300

    Debug

--- app/Deliveries.php
+++ app/Deliveries.php
@@ -74,7 +74,7 @@ class Deliveries extends BaseModel
                            'year' => date('Y', strtotime("+1 Month", strtotime($year.'-'.$month))),
                            'month' => date('m', strtotime("+1 Month", strtotime($year.'-'.$month))),
                         ];
-      // return $latestdate;
+      return $latestdate;
       dd($this->selectRaw("month(datereceived) as month, year, datereceived, id")->where('year', '=', $year)->get()->toArray());
       $limit = date('Y-m', strtotime("-1 Month", strtotime(date('Y-m'))));
       $currentloopdate = $latestdate->year . '-' . $latestdate->month;
