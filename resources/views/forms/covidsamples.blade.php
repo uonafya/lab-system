eb593a68cc6df76756f8266f94599cacba0ad5e3
Joel <joelkith@gmail.com>
2020-06-19 09:41:40 +0300

    Updates

--- resources/views/forms/covidsamples.blade.php
+++ resources/views/forms/covidsamples.blade.php
@@ -139,11 +139,7 @@
 
                         @include('partial.date', ['model' => $m, 'prop' => 'dob', 'label' => 'Date of Birth', 'default_val' => $sample->patient->dob ?? null, 'class' => 'date-dob'])
 
-                        @if(env('APP_LAB') == 4)
-                            @include('partial.input', ['model' => $m, 'required' => true, 'prop' => 'age', 'is_number' => true, 'label' => 'Age'])
-                        @else
-                            @include('partial.input', ['model' => $m, 'prop' => 'age', 'is_number' => true, 'label' => 'Age'])
-                        @endif
+                        @include('partial.input', ['model' => $m, 'required' => true, 'prop' => 'age', 'is_number' => true, 'label' => 'Age'])
 
                         @include('partial.select', ['model' => $m, 'prop' => 'sex', 'default_val' => $sample->patient->sex ?? null, 'required' => true, 'label' => 'Sex', 'items' => $gender, 'prop2' => 'gender_description'])
 
