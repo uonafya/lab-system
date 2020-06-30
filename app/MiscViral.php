8ca3fd2194c41a794ac190ec5b271df9f81d3dae
Joel <joelkith@gmail.com>
2020-06-30 16:33:06 +0300

    Altered PDFs

--- app/MiscViral.php
+++ app/MiscViral.php
@@ -1064,7 +1064,7 @@ class MiscViral extends Common
         $prophylaxis = \DB::table('viralregimen')->get();
         $justifications = \DB::table('viraljustifications')->orderBy('rank', 'asc')->where('flag', 1)->get();
 
-        $min_date = date('Y-m-d', strtotime('-3 weeks'));
+        $min_date = date('Y-m-d', strtotime('-1 week'));
         $samples = ViralsampleView::join('view_facilitys', 'view_facilitys.id', '=', 'viralsamples_view.facility_id')
                 ->select('viralsamples_view.*')
                 ->where(['repeatt' => 0, 'county_id' => 17])
