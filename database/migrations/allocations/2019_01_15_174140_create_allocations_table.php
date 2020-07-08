32b6fc847f3b4b4c28c63fc4c2ee7da97a35dc0c
Joshua Bakasa <baksajoshua09@gmail.com>
2020-07-08 11:54:49 +0300

    Added Column to differentiate between covid allowed and EID allowed

--- database/migrations/allocations/2019_01_15_174140_create_allocations_table.php
+++ database/migrations/allocations/2019_01_15_174140_create_allocations_table.php
@@ -13,26 +13,26 @@ class CreateAllocationsTable extends Migration
      */
     public function up()
     {
-        Schema::create('allocations', function(Blueprint $table){
-            $table->bigIncrements('id');
-            $table->bigInteger('national_id')->nullable();
-            $table->integer('machine_id');
-            $table->tinyInteger('testtype');
-            $table->integer('year');
-            $table->tinyInteger('month');
-            $table->string('order_num', 20)->after('month')->nullable();
-            $table->date('datesubmitted')->nullable();
-            $table->string('submittedby', 100)->nullable();
-            $table->tinyInteger('lab_id')->nullable();
-            $table->text('allocationcomments')->comment("Comment section filled at the Lab");
-            $table->text('issuedcomments')->comment("Comment section filed at National");
-            $table->tinyInteger('approve')->default(0);
-            $table->text('disapprovereason')->nullable();
-            $table->tinyInteger('synched')->default(0)->comment("0:Awaiting synching; 1:Synched; 2:Update awaiting synching;");
-            $table->date('datesynched')->nullable();
-            $table->softDeletes();
-            $table->timestamps();
-        });
+        // Schema::create('allocations', function(Blueprint $table){
+        //     $table->bigIncrements('id');
+        //     $table->bigInteger('national_id')->nullable();
+        //     $table->integer('machine_id');
+        //     $table->tinyInteger('testtype');
+        //     $table->integer('year');
+        //     $table->tinyInteger('month');
+        //     $table->string('order_num', 20)->after('month')->nullable();
+        //     $table->date('datesubmitted')->nullable();
+        //     $table->string('submittedby', 100)->nullable();
+        //     $table->tinyInteger('lab_id')->nullable();
+        //     $table->text('allocationcomments')->comment("Comment section filled at the Lab");
+        //     $table->text('issuedcomments')->comment("Comment section filed at National");
+        //     $table->tinyInteger('approve')->default(0);
+        //     $table->text('disapprovereason')->nullable();
+        //     $table->tinyInteger('synched')->default(0)->comment("0:Awaiting synching; 1:Synched; 2:Update awaiting synching;");
+        //     $table->date('datesynched')->nullable();
+        //     $table->softDeletes();
+        //     $table->timestamps();
+        // });
     }
 
     /**
