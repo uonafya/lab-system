9e6742048c3a98aa919865917ba1131e8833ec8e
Joshua Bakasa <baksajoshua09@gmail.com>
2020-07-10 16:33:03 +0300

    Updated the COVID kits

---
+++ database/migrations/2020_07_10_155658_alter_covid_kits_add_manual_machine.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterCovidKitsAddManualMachine extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('covid_kits', function (Blueprint $table) {
+            $table->tinyInteger('manual_machine')->nullable()->after('machine');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
