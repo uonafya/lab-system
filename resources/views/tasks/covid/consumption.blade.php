c33f1c399e174b0fd905f5fe4e95bcc0710363da
Joshua Bakasa <baksajoshua09@gmail.com>
2020-07-20 16:08:19 +0300

    Debug

--- resources/views/tasks/covid/consumption.blade.php
+++ resources/views/tasks/covid/consumption.blade.php
@@ -27,7 +27,7 @@
         @php
             $machine = null;
             if (null !== trim($machinekey)){
-               print_r($machine);
+               print_r($machinekey);
                $machine = \App\Machine::find($machinekey);
                $machinename = $machine->machine . ' Kits';
             } else {
