2c17aa501b46dee04d0ed19ba86bf1f8f37f08d7
Joel <joelkith@gmail.com>
2020-07-22 11:15:35 +0300

    Updates

--- routes/web.php
+++ routes/web.php
@@ -171,8 +171,8 @@ Route::middleware(['auth'])->group(function(){
 				Route::get('knh/upload', 'CovidSampleController@knh_sample_page');
 				Route::post('knh/upload', 'CovidSampleController@upload_knh_samples');
 				
-				Route::get('nairobi/upload', 'CovidSampleController@knh_sample_page');
-				Route::post('nairobi/upload', 'CovidSampleController@upload_knh_samples');
+				Route::get('nairobi/upload', 'CovidSampleController@nairobi_sample_page');
+				Route::post('nairobi/upload', 'CovidSampleController@upload_nairobi_samples');
 				
 				Route::post('transfer', 'CovidSampleController@transfer');
 			});
