03f9e2237d12648c21e920b3453de16be0ad3d70
Joel <joelkith@gmail.com>
2020-08-14 14:33:40 +0300

    Updates

--- app/CovidModels/BaseModel.php
+++ app/CovidModels/BaseModel.php
@@ -18,4 +18,36 @@ class BaseModel extends Model
         }
         return '';
     }
+    public function get_link($attr)
+    {
+        $user = auth()->user();
+        $c = $this->route_name;
+
+        $pre = '';
+        if(\Str::contains($c, 'viral')) $pre = 'viral';
+        if(\Str::contains($c, 'dr')) $pre = 'dr_';
+        if(\Str::contains($c, 'covid')) $pre = 'covid_';
+        $user = auth()->user();
+
+        if(\Str::contains($attr, 'extraction')) $url = url('dr_extraction_worksheet/gel_documentation/' . $this->$attr);
+        else if(\Str::contains($attr, 'worksheet')) $url = url($pre . 'worksheet/approve/' . $this->$attr);
+        // else if(\Str::contains($attr, 'sample') || (\Str::contains($c, 'sample') && $attr == 'id')) $url = url($c . '/runs/' . $this->$attr);
+        else{
+            $a = explode('_', $attr);
+            $url = url($pre . $a[0] . '/' . $this->$attr);
+            // if(\Str::contains($c, 'patient')) $url = url($pre . $a[0] . '/' . $this->patient_id);
+        }
+
+        if($attr == 'id' && (!$user || ($user && $user->user_type_id == 5))) return null;
+
+        if(\Str::contains($attr, ['worksheet', 'sample']) && (!$user || ($user && $user->user_type_id == 5))) return $this->$attr;
+
+        $text = $this->$attr;
+
+        if(\Str::contains($c, 'patient')) $text = $this->patient;
+
+        $full_link = "<a href='{$url}' target='_blank'> {$text} </a>";
+
+        return $full_link;
+    }
 }
