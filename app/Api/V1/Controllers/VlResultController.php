07cb432057bb6276b410547e23a153d10bdece78
Joel <joelkith@gmail.com>
2020-09-07 10:28:45 +0300

    Updates

--- app/Api/V1/Controllers/VlResultController.php
+++ app/Api/V1/Controllers/VlResultController.php
@@ -25,6 +25,7 @@ class VlResultController extends BaseController
 
     public function result(CovidRequest $request)
     {
+        if(env('APP_LAB') != 1) abort(400, 'Wrong lab');
         $sample_id = $request->input('Sample Id');
         $datetested = $request->input('Test Datetime');
         $result = $request->input('Result');
@@ -33,11 +34,19 @@ class VlResultController extends BaseController
 
         $result_array = MiscViral::sample_result($result, $error, $units);
         $sample = Viralsample::find($sample_id);
-        if(!$sample) continue;
+        if(!$sample) abort(404);
+        if($sample->dateapproved){
+            return response()->json([
+              'status' => 'not ok',
+              'message' => 'Sample has already been approved',
+            ], 400);
+        }
         $sample->fill($result_array);
         $sample->datetested = date('Y-m-d', strtotime($datetested));
         if($sample->datetested == '1970-01-01') $sample->datetested = date('Y-m-d');
         $sample->save();
+
+
     }
 
 }
