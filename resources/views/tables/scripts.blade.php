120a2952421f11f18d480c3561a6343041acc429
Joel <joelkith@gmail.com>
2020-10-01 21:17:16 +0300

    Updates

--- resources/views/tables/scripts.blade.php
+++ resources/views/tables/scripts.blade.php
@@ -49,6 +49,30 @@
             ]
         });
 
+        $('.multi-filter tfoot th').each( function () {
+            var title = $(this).text();
+            $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
+        } );
+
+        // DataTable
+        var table = $('.multi-filter').DataTable({
+            initComplete: function () {
+                // Apply the search
+                this.api().columns().every( function () {
+                    var that = this;
+     
+                    $( 'input', this.footer() ).on( 'keyup change clear', function () {
+                        if ( that.search() !== this.value ) {
+                            that
+                                .search( this.value )
+                                .draw();
+                        }
+                    } );
+                } );
+            }
+        });
+
+
         var msg;
         var dynamicErrorMsg = function () { return msg; }
         
