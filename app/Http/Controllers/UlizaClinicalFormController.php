6ba2d564cdc85ec3bf4c1fe6ebfdc19eeff5c768
Joel <joelkith@gmail.com>
2020-10-22 13:34:15 +0300

    Updates

--- app/Http/Controllers/UlizaClinicalFormController.php
+++ app/Http/Controllers/UlizaClinicalFormController.php
@@ -103,7 +103,11 @@ class UlizaClinicalFormController extends Controller
     {
         // $ulizaClinicalForm->entry_pdf(null, true);
         $ulizaClinicalForm = UlizaClinicalForm::find($id);
-        \App\UlizaPage::entry_pdf($ulizaClinicalForm, null, true);
+        // \App\UlizaPage::entry_pdf($ulizaClinicalForm, null, true);
+
+        $reasons = DB::table('uliza_reasons')->where('public', 1)->get();
+        $regimens = DB::table('viralregimen')->get();
+        view('uliza.exports.clinical_form', compact('reasons', 'regimens', 'ulizaClinicalForm'))
     }
 
     /**
