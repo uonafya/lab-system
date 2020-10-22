ddf346073af0255578b8cea803c6c06f4a77367f
Joel <joelkith@gmail.com>
2020-10-22 13:08:13 +0300

    Updates

--- resources/views/uliza/exports/clinical_form.blade.php
+++ resources/views/uliza/exports/clinical_form.blade.php
@@ -1,7 +1,8 @@
 <html>
 
 <head>
-    <link rel="stylesheet" href="{{ public_path('vendor/bootstrap/dist/css/bootstrap.css') }}" />	
+    <!-- <link rel="stylesheet" href="{{ public_path('vendor/bootstrap/dist/css/bootstrap.css') }}" />	 -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 </head>
 
 
@@ -398,7 +399,9 @@
 	</div>
 </body>
 
-<script src="{{ public_path('vendor/bootstrap/dist/js/bootstrap.min.js') }}"></script>
-<script src="{{ public_path('vendor/jquery/dist/jquery.min.js') }}"></script>
+<!-- <script src="{{ public_path('vendor/bootstrap/dist/js/bootstrap.min.js') }}"></script> -->
+<!-- <script src="{{ public_path('vendor/jquery/dist/jquery.min.js') }}"></script> -->
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
 </html>
