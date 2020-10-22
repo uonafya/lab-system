9315c1c3d3ff23d034d51c96e611d0d92fd6b21a
Joel <joelkith@gmail.com>
2020-10-22 12:34:23 +0300

    Updates

--- app/UlizaClinicalForm.php
+++ app/UlizaClinicalForm.php
@@ -3,6 +3,8 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Mpdf\Mpdf;
+use DB;
 
 class UlizaClinicalForm extends BaseModel
 {
@@ -50,6 +52,34 @@ class UlizaClinicalForm extends BaseModel
     public function getSubjectIdentifierAttribute()
     {
         return 'CCC#: ' . $this->cccno . ' Nat#: ' . $this->nat_number;
+    }
+
+    public function getEntryPathAttribute()
+    {
+        return storage_path('app/batches/uliza/entry-' . $this->id . '.pdf');
+    }
+
+    public function entry_pdf($file_path=null, $download=false)
+    {
+        if(!$file_path) $file_path = $this->entry_path;
+
+        if(file_exists($file_path)) unlink($file_path);
+
+        $file_name = explode('/', $file_path);
+        $file_name = array_pop($file_name);
+
+
+        $mpdf = new Mpdf();
+        $reasons = DB::table('uliza_reasons')->where('public', 1)->get();
+        $regimens = DB::table('viralregimen')->get();
+        $ulizaClinicalForm = $this;
+        $view_data = view('uliza.exports.clinical_form', compact('reasons', 'regimens', 'ulizaClinicalForm'))->render();
+        $mpdf->WriteHTML($view_data);
+
+        if($download) return $mpdf->Output($this->individual_path, \Mpdf\Output\Destination::DOWNLOAD);
+        else{
+            $mpdf->Output($this->individual_path, \Mpdf\Output\Destination::FILE);
+        }
     }
 
 }
--- app/UlizaClinicalVisit.php
+++ app/UlizaClinicalVisit.php
@@ -3,8 +3,6 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Mpdf\Mpdf;
-use DB;
 
 class UlizaClinicalVisit extends BaseModel
 {
@@ -13,34 +11,6 @@ class UlizaClinicalVisit extends BaseModel
     public function clinical_form()
     {
         return $this->belongsTo('App\UlizaClinicalForm');
-    }
-
-    public function getEntryPathAttribute()
-    {
-    	return storage_path('app/batches/uliza/entry-' . $this->id . '.pdf');
-    }
-
-    public function entry_pdf($file_path=null, $download=false)
-    {
-    	if(!$file_path) $file_path = $this->entry_path;
-
-    	if(file_exists($file_path)) unlink($file_path);
-
-    	$file_name = explode('/', $file_path);
-    	$file_name = array_pop($file_name);
-
-
-        $mpdf = new Mpdf();
-        $reasons = DB::table('uliza_reasons')->where('public', 1)->get();
-        $regimens = DB::table('viralregimen')->get();
-        $ulizaClinicalForm = $this;
-        $view_data = view('uliza.exports.clinical_form', compact('reasons', 'regimens', 'ulizaClinicalForm'))->render();
-        $mpdf->WriteHTML($view_data);
-
-        if($download) return $mpdf->Output($this->individual_path, \Mpdf\Output\Destination::DOWNLOAD);
-        else{
-	        $mpdf->Output($this->individual_path, \Mpdf\Output\Destination::FILE);
-        }
     }
 
 }
