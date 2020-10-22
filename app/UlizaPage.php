84572a3e34b0ccdcd142b21b94908cad751ed517
Joel <joelkith@gmail.com>
2020-10-22 13:21:07 +0300

    Updates

--- app/UlizaPage.php
+++ app/UlizaPage.php
@@ -3,6 +3,8 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Mpdf\Mpdf;
+use DB;
 
 class UlizaPage extends Model
 {
@@ -156,6 +158,28 @@ class UlizaPage extends Model
         ]);
     }
 
+
+    public function entry_pdf($ulizaClinicalForm, $file_path=null, $download=false)
+    {
+        if(!$file_path) $file_path = $ulizaClinicalForm->entry_path;
+
+        if(file_exists($file_path)) unlink($file_path);
+
+        $file_name = explode('/', $file_path);
+        $file_name = array_pop($file_name);
+
+
+        $mpdf = new Mpdf();
+        $reasons = DB::table('uliza_reasons')->where('public', 1)->get();
+        $regimens = DB::table('viralregimen')->get();
+        $view_data = view('uliza.exports.clinical_form', compact('reasons', 'regimens', 'ulizaClinicalForm'))->render();
+        $mpdf->WriteHTML($view_data);
+
+        if($download) return $mpdf->Output($file_name, \Mpdf\Output\Destination::DOWNLOAD);
+        else{
+            $mpdf->Output($file_path, \Mpdf\Output\Destination::FILE);
+        }
+    }
 
 
 }
