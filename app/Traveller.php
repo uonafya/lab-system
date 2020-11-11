57eaae5b51e3c4a016621dce21f24bbeee4e8e42
Joel <joelkith@gmail.com>
2020-11-11 20:55:38 +0300

    Updates

--- app/Traveller.php
+++ app/Traveller.php
@@ -30,41 +30,41 @@ class Traveller extends BaseModel
 
     public function setIgmResultAttribute($value)
     {
-        if(is_numeric($value) || !$value) $this->attributes['result'] = $value;
+        if(is_numeric($value) || !$value) $this->attributes['igm_result'] = $value;
         else{
             $value = strtolower($value);
-            if(\Str::contains($value, ['neg'])) $this->attributes['result'] = 1;
-            else if(\Str::contains($value, ['pos'])) $this->attributes['result'] = 2;
-            else if(\Str::contains($value, ['coll'])) $this->attributes['result'] = 5;
+            if(\Str::contains($value, ['neg'])) $this->attributes['igm_result'] = 1;
+            else if(\Str::contains($value, ['pos'])) $this->attributes['igm_result'] = 2;
+            else if(\Str::contains($value, ['coll'])) $this->attributes['igm_result'] = 5;
         }
     }
 
     public function getIgmResultNameAttribute()
     {
-        if($this->result == 1){ return "Negative"; }
-        else if($this->result == 2){ return "Positive"; }
-        else if($this->result == 3){ return "Failed"; }
-        else if($this->result == 5){ return "Collect New Sample"; }
+        if($this->igm_result == 1){ return "Negative"; }
+        else if($this->igm_result == 2){ return "Positive"; }
+        else if($this->igm_result == 3){ return "Failed"; }
+        else if($this->igm_result == 5){ return "Collect New Sample"; }
         else{ return ""; }
     }
 
     public function setIggIgmResultAttribute($value)
     {
-        if(is_numeric($value) || !$value) $this->attributes['result'] = $value;
+        if(is_numeric($value) || !$value) $this->attributes['igg_igm_result'] = $value;
         else{
             $value = strtolower($value);
-            if(\Str::contains($value, ['neg'])) $this->attributes['result'] = 1;
-            else if(\Str::contains($value, ['pos'])) $this->attributes['result'] = 2;
-            else if(\Str::contains($value, ['coll'])) $this->attributes['result'] = 5;
+            if(\Str::contains($value, ['neg'])) $this->attributes['igg_igm_result'] = 1;
+            else if(\Str::contains($value, ['pos'])) $this->attributes['igg_igm_result'] = 2;
+            else if(\Str::contains($value, ['coll'])) $this->attributes['igg_igm_result'] = 5;
         }
     }
 
     public function getIggIgmResultNameAttribute()
     {
-        if($this->result == 1){ return "Negative"; }
-        else if($this->result == 2){ return "Positive"; }
-        else if($this->result == 3){ return "Failed"; }
-        else if($this->result == 5){ return "Collect New Sample"; }
+        if($this->igg_igm_result == 1){ return "Negative"; }
+        else if($this->igg_igm_result == 2){ return "Positive"; }
+        else if($this->igg_igm_result == 3){ return "Failed"; }
+        else if($this->igg_igm_result == 5){ return "Collect New Sample"; }
         else{ return ""; }
     }
 
