20662dbe122598760c95d282d5b2b9edbe679c79
Joel <joelkith@gmail.com>
2020-11-26 14:51:25 +0300

    Updates

--- app/Imports/AlupeCovidImport.php
+++ app/Imports/AlupeCovidImport.php
@@ -103,6 +103,13 @@ class AlupeCovidImport implements OnEachRow, WithHeadingRow
         if($datereceived == '1970-01-01') $datereceived = date('Y-m-d');
 
         $sample = CovidSample::where(['patient_id' => $p->id, 'datecollected' => $datecollected])->first();
+        if(!$sample && !auth()->user()->user_type_id){
+            $sample = CovidSample::where(['patient_id' => $p->id])
+            ->whereBetween('datecollected', [
+                date('Y-m-d', strtotime($datecollected . ' -6days')), 
+                date('Y-m-d', strtotime($datecollected . ' +6days')), 
+            ])->first();
+        }
         if(!$sample) $sample = new CovidSample;
 
         $test_type = $row->test_type ?? 'initial';
