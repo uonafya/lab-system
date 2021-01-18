b40600c6ed7168cd90954e8980b2770dc4fbbcd7
Your Name <joelkith@gmail.com>
2021-01-18 16:02:58 +0300

    Updates

--- app/MiscDr.php
+++ app/MiscDr.php
@@ -912,9 +912,9 @@ class MiscDr extends Common
 
 					if(!$patient) dd('Patient ' . $seq_file . ' ID ' . $id . ' not found');
 
-					// $sample = $patient->dr_sample()->whereNull('worksheet_id')->first();	
+					$sample = $patient->dr_sample()->whereNull('worksheet_id')->first();	
 					// $sample = $patient->dr_sample()->whereNotNull('extraction_worksheet_id')->first();	
-					$sample = $patient->dr_sample()->first();	
+					// $sample = $patient->dr_sample()->first();	
 					// $sample->worksheet_id = $drWorksheet->id;
 					// $sample->save();			
 
