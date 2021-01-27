12fca1e17a839518a14c124e997886fcae58581e
Your Name <joelkith@gmail.com>
2021-01-27 09:31:09 +0300

    Updates

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -561,7 +561,7 @@ class ReportController extends Controller
         if ($request->input('types') == 'failed'){
             $model = $model->when($testtype, function($query) use ($testtype){
                                 if ($testtype == 'EID')
-                                    return $query->whereIn('result', [5])->where('repeatt', '=', 1);
+                                    return $query->whereIn('result', [3])->where('repeatt', '=', 1);
                                 if ($testtype == 'VL')
                                     return $query->where('repeatt', '=', 1)->whereIn('result', ['Failed']);
                             });
