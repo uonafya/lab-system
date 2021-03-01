82f5a26af85b57fa61f431ae74afad76b51d50bb
Joshua Bakasa <baksajoshua09@gmail.com>
2021-03-01 09:56:32 +0300

    Update to view the consumption report

--- resources/views/reports/consumptionreport.blade.php
+++ resources/views/reports/consumptionreport.blade.php
@@ -34,7 +34,7 @@
         <div class="col-lg-12">
             <div class="hpanel">
                 <div class="panel-body" style="padding: 20px;box-shadow: none; border-radius: 0px;">
-                    @if(Auth::user()->eidvl_consumption_allowed)
+                    {{-- @if(Auth::user()->eidvl_consumption_allowed) --}}
                     <form method="post" action="{{ url('reports/kitsconsumption/update') }}">
                         @csrf
                         @foreach($request as $key => $value)
@@ -44,7 +44,7 @@
                             <button class="btn btn-primary" type="submit">Update Consumption Details</button>
                         </div>
                     </form>
-                    @endif
+                    {{-- @endif --}}
                 	<table class="table table-striped table-bordered table-hover" style="font-size: 10px;margin-top: 1em;">
                 		<thead>
                 			<tr>
