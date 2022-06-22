47bac36d40cf6c77bf422259ff1a0ccc4d47ae32
ian-njuguna11 <njugunaayan@gmail.com>
2022-06-22 16:19:49 +0300

    partials upi

---
+++ .history/resources/views/forms/partials/upi.blade_20220622161754.php
@@ -0,0 +1,8 @@
+<div class="form-group">
+    <label class="col-sm-4 control-label">Patient UPI No from partials.
+        <strong><div style='color: #ff0000; display: inline;'>*</div></strong>
+    </label>
+    <div class="col-sm-3">
+        <input class="form-control" id="name" name="patient_upi" type="text" required>
+    </div>
+</div>
