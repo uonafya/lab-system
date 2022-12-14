fb25a280cee2365e1c968ddfb26e696391080777
faithchep <faithchepkoech40@gmail.com>
2022-12-14 16:33:50 +0300

    note

--- app/Mail/UntestedSamples.php
+++ app/Mail/UntestedSamples.php
@@ -1,48 +1,4 @@
 <?php
-//
-//namespace App\Mail;
-//
-//use Illuminate\Bus\Queueable;
-//use Illuminate\Mail\Mailable;
-//use Illuminate\Queue\SerializesModels;
-//use Illuminate\Contracts\Queue\ShouldQueue;
-//
-//use DB;
-//
-//use App\Lookup;
-//use App\Lab;
-//
-//class UntestedSamples extends Mailable
-//{
-//    use Queueable, SerializesModels;
-//
-//    public $data;
-//
-//    /**
-//     * Create a new message instance.
-//     *
-//     * @return void
-//     */
-//    public function __construct($data)
-//    {
-//
-//        $this->data =$data;
-//
-//    }
-//
-//    /**
-//     * Build the message.
-//     *
-//     * @return $this
-//     */
-//    public function build()
-//    {
-//        $this->attach($this->data);
-//
-//        return $this->subject("UNTESTED SAMPLES")->view('emails.untested');
-//    }
-//}
-
 
 namespace App\Mail;
 
