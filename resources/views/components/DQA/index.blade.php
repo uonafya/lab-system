f2fb97ed16f471fa3d41a0dd6b237b4c3a2191a1
@yn <njugunaayan@gmail.com>
2023-03-02 17:40:29 +0300

    bootstraping dqa module
    bootstraping dqa module

--- resources/views/components/DQA/index.blade.php
+++ resources/views/components/DQA/index.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta http-equiv="Content-Language" content="en">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <title>Analytics Dashboard - This is an example dashboard created using build-in elements and components.</title>
+    <title>DQA Dashboard</title>
     <meta name="viewport"
         content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no">
     <meta name="description" content="This is an example dashboard created using build-in elements and components.">
